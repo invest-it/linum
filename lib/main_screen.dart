1e562db2f1d2a0aa07bebcf373ddbe141d78067f
Marc Nickel <marc@zubik.io>
2021-12-04 18:07:31 +0100

    Final push of the day

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -35,51 +35,6 @@ class MainScreen extends StatelessWidget {
                     ),
                   ],
                 ),
-                Positioned(
-                  top: 110,
-                  left: 10,
-                  right: 10,
-                  child: Column(
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Container(
-                          width: MediaQuery.of(context).size.width * 0.85,
-                          height: MediaQuery.of(context).size.height * 0.20,
-                          color: Colors.grey[100],
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: <Widget>[
-                              Text(
-                                'Aktueller Kontostand',
-                                style: TextStyle(fontSize: 20),
-                              ),
-                              Text('Datum'),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                                children: [
-                                  Text(
-                                    monthlyBudget.toString(),
-                                    style: TextStyle(fontSize: 20),
-                                  ),
-                                  Container(
-                                    width: 1,
-                                  ),
-                                  Text(
-                                    '€',
-                                    style: TextStyle(fontSize: 16),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
               ],
             );
             /*ListView(
