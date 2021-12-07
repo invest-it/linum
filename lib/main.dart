8d361640f7f7f71b57e98f726bb6f70e5328d3ee
Soencke Evers <Soencke.Evers@web.de>
2021-12-07 19:11:15 +0100

    Refactored to a folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:linum/main_screen.dart';
+import 'package:linum/screens/main_screen.dart';
 import 'package:linum/providers/authentication_service.dart';
 import 'package:provider/provider.dart';
 import 'dart:developer';
