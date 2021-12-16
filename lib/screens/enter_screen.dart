d7de79de34f8c9b707f764f86f524544a81a04c1
Marc Nickel <marc@zubik.io>
2021-12-16 15:52:52 +0100

    Worked on the list in transactions

--- lib/screens/enter_screen.dart
+++ lib/screens/enter_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:linum/widgets/enter_screen_list.dart';
 import 'package:linum/widgets/text_container.dart';
 
 class EnterScreen extends StatefulWidget {
@@ -33,151 +34,150 @@ class _EnterScreenState extends State<EnterScreen> {
     return GestureDetector(
       onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
       child: Scaffold(
-        body: Stack(
-          children: <Widget>[
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                //the top, green lip
-                ClipRRect(
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.zero,
-                    bottom: Radius.circular(40),
-                  ),
-                  child: GestureDetector(
-                    child: Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.height * 0.20,
-                      color: Theme.of(context).colorScheme.primary,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          SizedBox(
-                            height: MediaQuery.of(context).size.height * 0.06,
+        body: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              //the top, green lip
+              ClipRRect(
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.zero,
+                  bottom: Radius.circular(40),
+                ),
+                child: GestureDetector(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height * 0.20,
+                    color: Theme.of(context).colorScheme.primary,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.06,
+                        ),
+                        //TO DO: elimante the small bubble below the disabled color
+                        //Change background color to have a better view
+                        TextField(
+                          controller: myController,
+                          showCursor: false,
+                          keyboardType: TextInputType.number,
+                          textAlign: TextAlign.center,
+                          decoration: InputDecoration(
+                            hintText: isExpenses ? "-" + " 0.0" : "+" + " 0.0",
+                            hintStyle: TextStyle(
+                              color:
+                                  _colorPicker(isExpenses, isIncome, context),
+                            ),
+                            border: InputBorder.none,
+                            focusedBorder: InputBorder.none,
                           ),
-                          //TO DO: elimante the small bubble below the disabled color
-                          //Change background color to have a better view
-                          TextField(
-                            controller: myController,
-                            showCursor: false,
-                            keyboardType: TextInputType.number,
-                            textAlign: TextAlign.center,
-                            decoration: InputDecoration(
-                              hintText: "0.0",
-                              hintStyle: TextStyle(
-                                color:
-                                    _colorPicker(isExpenses, isIncome, context),
+                          style: TextStyle(
+                              color:
+                                  _colorPicker(isExpenses, isIncome, context),
+                              fontSize: 30),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.75,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    isExpenses = true;
+                                    isIncome = false;
+                                    isTransaction = false;
+                                  });
+                                },
+                                child: Container(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.25,
+                                  child: isExpenses
+                                      ? TextContainer(
+                                          //context: context,
+                                          transactionClass: "Expenses",
+                                          isExpenses: isExpenses,
+                                          isIncome: isIncome,
+                                        )
+                                      : Center(
+                                          child: Text(
+                                          "Expenses",
+                                          style: TextStyle(
+                                              color: Theme.of(context)
+                                                  .colorScheme
+                                                  .background),
+                                        )),
+                                ),
                               ),
-                              border: InputBorder.none,
-                              focusedBorder: InputBorder.none,
-                            ),
-                            style: TextStyle(
-                                color:
-                                    _colorPicker(isExpenses, isIncome, context),
-                                fontSize: 30),
-                          ),
-                          Container(
-                            width: MediaQuery.of(context).size.width * 0.75,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                GestureDetector(
-                                  onTap: () {
-                                    setState(() {
-                                      isExpenses = true;
-                                      isIncome = false;
-                                      isTransaction = false;
-                                    });
-                                  },
-                                  child: Container(
-                                    width: MediaQuery.of(context).size.width *
-                                        0.25,
-                                    child: isExpenses
-                                        ? TextContainer(
-                                            //context: context,
-                                            transactionClass: "Expenses",
-                                            isExpenses: isExpenses,
-                                            isIncome: isIncome,
-                                          )
-                                        : Center(
-                                            child: Text(
-                                            "Expenses",
+                              GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    isExpenses = false;
+                                    isIncome = true;
+                                    isTransaction = false;
+                                  });
+                                },
+                                child: Container(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.25,
+                                  child: isIncome
+                                      ? TextContainer(
+                                          //context: context,
+                                          transactionClass: "Income",
+                                          isExpenses: isExpenses,
+                                          isIncome: isIncome,
+                                        )
+                                      : Center(
+                                          child: Text(
+                                          "Income",
+                                          style: TextStyle(
+                                              color: Theme.of(context)
+                                                  .colorScheme
+                                                  .background),
+                                        )),
+                                ),
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    isExpenses = false;
+                                    isIncome = false;
+                                    isTransaction = true;
+                                  });
+                                },
+                                child: Container(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.25,
+                                  child: isTransaction
+                                      ? TextContainer(
+                                          //context: context,
+                                          transactionClass: "Transaction",
+                                          isExpenses: isExpenses,
+                                          isIncome: isIncome,
+                                        )
+                                      : Center(
+                                          child: Text(
+                                            "Transaction",
                                             style: TextStyle(
                                                 color: Theme.of(context)
                                                     .colorScheme
                                                     .background),
-                                          )),
-                                  ),
-                                ),
-                                GestureDetector(
-                                  onTap: () {
-                                    setState(() {
-                                      isExpenses = false;
-                                      isIncome = true;
-                                      isTransaction = false;
-                                    });
-                                  },
-                                  child: Container(
-                                    width: MediaQuery.of(context).size.width *
-                                        0.25,
-                                    child: isIncome
-                                        ? TextContainer(
-                                            //context: context,
-                                            transactionClass: "Income",
-                                            isExpenses: isExpenses,
-                                            isIncome: isIncome,
-                                          )
-                                        : Center(
-                                            child: Text(
-                                            "Income",
-                                            style: TextStyle(
-                                                color: Theme.of(context)
-                                                    .colorScheme
-                                                    .background),
-                                          )),
-                                  ),
-                                ),
-                                GestureDetector(
-                                  onTap: () {
-                                    setState(() {
-                                      isExpenses = false;
-                                      isIncome = false;
-                                      isTransaction = true;
-                                    });
-                                  },
-                                  child: Container(
-                                    width: MediaQuery.of(context).size.width *
-                                        0.25,
-                                    child: isTransaction
-                                        ? TextContainer(
-                                            //context: context,
-                                            transactionClass: "Transaction",
-                                            isExpenses: isExpenses,
-                                            isIncome: isIncome,
-                                          )
-                                        : Center(
-                                            child: Text(
-                                              "Transaction",
-                                              style: TextStyle(
-                                                  color: Theme.of(context)
-                                                      .colorScheme
-                                                      .background),
-                                            ),
                                           ),
-                                  ),
+                                        ),
                                 ),
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
-              ],
-            ),
-          ],
+              ),
+              EnterScreenList(isExpenses: isExpenses, isIncome: isIncome)
+            ],
+          ),
         ),
       ),
     );
@@ -188,7 +188,7 @@ _colorPicker(bool isExpenses, bool isIncome, context) {
   if (isExpenses) {
     return Theme.of(context).colorScheme.error;
   } else if (isIncome) {
-    return Theme.of(context).colorScheme.primary;
+    return Theme.of(context).colorScheme.background;
   }
   return Theme.of(context).colorScheme.secondary;
 }
--- lib/widgets/enter_screen_list.dart
+++ lib/widgets/enter_screen_list.dart
@@ -3,28 +3,171 @@ import 'package:flutter/material.dart';
 class EnterScreenList extends StatelessWidget {
   final bool isExpenses;
   final bool isIncome;
+  List<Category> _categoriesExpenses = [
+    Category("Category", Icons.ac_unit),
+    Category("Account", Icons.create_new_folder),
+    Category("Date", Icons.access_alarm_outlined),
+    Category("Repeat", Icons.access_time_sharp),
+  ];
+  List<Category> _categoriesIncome = [
+    Category("Category", Icons.ac_unit),
+    Category("Account", Icons.create_new_folder),
+    Category("Date", Icons.access_alarm_outlined),
+    Category("Repeat", Icons.access_time_sharp),
+  ];
+  List<Category> _categoriesTransaction = [
+    Category("Account", Icons.create_new_folder),
+    Category("To Account", Icons.ac_unit),
+    Category("Date", Icons.access_alarm_outlined),
+    Category("Repeat", Icons.access_time_sharp),
+    Category("Beep", Icons.access_time_sharp),
+  ];
+  final List<int> colorCodes = <int>[600, 500, 100];
   EnterScreenList({Key? key, required this.isExpenses, required this.isIncome})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     if (isExpenses)
-      return ListView.builder(
-          itemCount: 6,
-          itemBuilder: (ctx, index) => Card(
-                elevation: 6,
-              ));
+      return Center(
+        child: Container(
+          height: 500,
+          width: MediaQuery.of(context).size.width * 0.8,
+          child: ListView.separated(
+            padding: const EdgeInsets.all(8),
+            itemCount: _categoriesExpenses.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                height: 50,
+                child: Row(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(
+                            color: Theme.of(context).colorScheme.background),
+                        borderRadius: BorderRadius.circular(5),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey,
+                            blurRadius: 2.0,
+                            spreadRadius: 0.0,
+                            offset: Offset(
+                                0.5, 2.0), // shadow direction: bottom right
+                          )
+                        ],
+                      ),
+                      child: Icon(_categoriesExpenses[index].icon),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(_categoriesExpenses[index].type + ":"),
+                  ],
+                ),
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) =>
+                const Divider(),
+          ),
+        ),
+      );
     else if (isIncome)
-      return ListView.builder(
-        itemBuilder: (ctx, index) => Card(
-          elevation: 6,
+      return Center(
+        child: Container(
+          height: 500,
+          width: MediaQuery.of(context).size.width * 0.8,
+          child: ListView.separated(
+            padding: const EdgeInsets.all(8),
+            itemCount: _categoriesIncome.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                height: 50,
+                child: Row(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(
+                            color: Theme.of(context).colorScheme.background),
+                        borderRadius: BorderRadius.circular(5),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey,
+                            blurRadius: 2.0,
+                            spreadRadius: 0.0,
+                            offset: Offset(
+                                0.5, 2.0), // shadow direction: bottom right
+                          )
+                        ],
+                      ),
+                      child: Icon(_categoriesIncome[index].icon),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(_categoriesIncome[index].type + ":"),
+                  ],
+                ),
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) =>
+                const Divider(),
+          ),
         ),
       );
     else
-      return ListView.builder(
-        itemBuilder: (ctx, index) => Card(
-          elevation: 6,
+      return Center(
+        child: Container(
+          height: 500,
+          width: MediaQuery.of(context).size.width * 0.8,
+          child: ListView.separated(
+            padding: const EdgeInsets.all(8),
+            itemCount: _categoriesTransaction.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                height: 50,
+                child: Row(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(
+                            color: Theme.of(context).colorScheme.background),
+                        borderRadius: BorderRadius.circular(5),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey,
+                            blurRadius: 2.0,
+                            spreadRadius: 0.0,
+                            offset: Offset(
+                                0.5, 2.0), // shadow direction: bottom right
+                          )
+                        ],
+                      ),
+                      child: Icon(_categoriesTransaction[index].icon),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(_categoriesTransaction[index].type + ":"),
+                  ],
+                ),
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) =>
+                const Divider(),
+          ),
         ),
       );
   }
+}
+
+class Category {
+  String type;
+  IconData icon;
+  Category(this.type, this.icon);
 }
