ed11114c545c0ffc6829b6ba356f42a529821c3a
Soencke Evers <Soencke.Evers@web.de>
2021-12-16 16:51:53 +0100

    small rework for statistics

--- lib/widgets/abstract/abstract_statistic_panel.dart
+++ lib/widgets/abstract/abstract_statistic_panel.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:linum/backend_functions/statistic_calculations.dart';
 
 abstract class AbstractStatisticPanel {
-  addStatisticData(Map<String, dynamic> statisticalData);
+  /// [statData] will calculate all needed Data. It also already has the filter implemented
+  /// It is possible that statData is null. In that case place some kind of loading screen or
+  /// everywhere a 0
+  addStatisticData(StatisticsCalculations? statData);
 
+  /// return the widget to display the data
   Widget get widget;
 }
