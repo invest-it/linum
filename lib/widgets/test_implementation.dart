10879466a2e5ff5759473503f4fb60ed32485f5f
Soencke Evers <Soencke.Evers@web.de>
2021-12-17 17:29:08 +0100

    test implementation bug fix, so it is a better template

--- lib/widgets/test_implementation.dart
+++ lib/widgets/test_implementation.dart
@@ -14,12 +14,12 @@ class TestListView implements BalanceDataListView {
 
   @override
   addBalanceData(List<dynamic> balanceData) {
-    if (balanceData[0] != null && balanceData[0]["Error"] != null) {
+    if (balanceData[0] != null && balanceData[0]["Error"] == null) {
       balanceData.forEach((arrayElement) {
         arrayElement.forEach((key, element) {
           if (key == "name") {
-            // log("key:  " + key);
-            // log("value: " + element.toString());
+            log("key:  " + key);
+            log("value: " + element.toString());
           }
         });
       });
