51adddde3a7f009f776da971f9c68f7b13afd7aa
Soencke Evers <Soencke.Evers@web.de>
2021-12-22 20:45:37 +0100

    multible jeans fix

--- lib/widgets/home_screen/home_screen_listview.dart
+++ lib/widgets/home_screen/home_screen_listview.dart
@@ -15,28 +15,20 @@ class HomeScreenListView implements BalanceDataListView {
 
   @override
   addBalanceData(List<dynamic> balanceData) {
+    log(balanceData.toString());
     List<Widget> list = [];
     if (balanceData[0] != null && balanceData[0]["Error"] == null) {
       balanceData.forEach(
         (arrayElement) {
-          arrayElement.forEach(
-            (key, element) {
-              // log("key:  " + key);
-              // log("value: " + element.toString());
-              //list.add(Text(key + ": " + element.toString()));
-
-              //list.add(ListTile(title: ),);
-              list.add(
-                GestureDetector(
-                  onTap: () => print(arrayElement["amount"].toString()),
-                  child: ListTile(
-                    title: Text(arrayElement["name"]),
-                    subtitle: Text((arrayElement["time"].toDate()).toString()),
-                    trailing: Text(arrayElement["amount"].toString() + "€"),
-                  ),
-                ),
-              );
-            },
+          list.add(
+            GestureDetector(
+              onTap: () => print(arrayElement["amount"].toString()),
+              child: ListTile(
+                title: Text(arrayElement["name"]),
+                subtitle: Text((arrayElement["time"].toDate()).toString()),
+                trailing: Text(arrayElement["amount"].toString() + "€"),
+              ),
+            ),
           );
         },
       );
