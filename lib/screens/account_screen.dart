16d82b57fff28748ebf214f7174f7a33be0ca0ee
aronzimmermann <aron.zimmermann@gmx.de>
2022-01-05 21:11:17 +0100

    Merge branch 'development' of github.com:VICFR1TZ/linum into development

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -11,13 +11,91 @@ class SettingsScreen extends StatefulWidget {
 
 class _SettingsScreenState extends State<SettingsScreen> {
   @override
+  // final Function ontap = CurrencyList();
+
   Widget build(BuildContext context) {
     BalanceDataProvider balanceDataProvider =
         Provider.of<BalanceDataProvider>(context);
     return Scaffold(
-      body: Center(
-        child: Text("Account"),
-      ),
-    );
+        body: Stack(
+      children: [
+        //Sceleton
+        Column(
+          children: <Widget>[
+            //column including upperLip and body of settings page
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                //the top, green lip
+                ClipRRect(
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.zero,
+                    bottom: Radius.circular(40),
+                  ),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height * 0.20,
+                    // TODO change this into a sustainable and responsive design
+
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+              ],
+            ),
+            //body of the settings page
+            Column(
+              children: [
+                //text: 'settings'
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 32.0,
+                    left: 16.0,
+                    right: 16.0,
+                    bottom: 0,
+                  ),
+                  //ListTile for selecting currencies
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Settings'),
+                      ListTile(
+                        // onTap: ontap(),
+                        title: Text('select currency'),
+                        trailing: Icon(
+                          Icons.arrow_drop_down,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                //line in between
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 16.0,
+                    right: 16.0,
+                    top: 0,
+                    bottom: 16.0,
+                  ),
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height * 0.003,
+                    child: const DecoratedBox(
+                      decoration: const BoxDecoration(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                ),
+                //going on...
+              ],
+            ),
+          ],
+        ),
+        //TODO: Account Logo
+      ],
+    ));
   }
 }
