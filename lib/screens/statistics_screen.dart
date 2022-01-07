7ed92b1c69a5b22f78c966adaa9961e03580b5b7
NightmindOfficial <mail@otismohr.de>
2022-01-07 16:51:02 +0100

    Implemented Screen_Skeleton in StatisticsScreen

--- lib/screens/statistics_screen.dart
+++ lib/screens/statistics_screen.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:linum/widgets/screen_skeleton/screen_skeleton.dart';
+import 'package:linum/widgets/text_container.dart';
 
 class BudgetScreen extends StatelessWidget {
   const BudgetScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Statistics"),
-    );
+    return ScreenSkeleton(
+        head: 'Stats',
+        body: Center(
+          child: Text('Statistics coming soon'),
+        ),
+        isInverted: true);
   }
 }
