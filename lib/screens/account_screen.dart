b9bbc575c198167995a90a5af311b0f5f3aa4b8e
Aron Zimmermann <aron.zimmermann@gmx.de>
2022-01-07 17:26:28 +0100

    Schwabenmodus

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:linum/providers/balance_data_provider.dart';
-import 'package:provider/provider.dart';
+// import 'package:linum/providers/balance_data_provider.dart';
+// import 'package:provider/provider.dart';
+// import 'package:linum/frontend_functions/materialcolor_creator.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({Key? key}) : super(key: key);
@@ -10,12 +11,14 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  bool _toggled = false;
+
   @override
   // final Function ontap = CurrencyList();
 
   Widget build(BuildContext context) {
-    BalanceDataProvider balanceDataProvider =
-        Provider.of<BalanceDataProvider>(context);
+    //BalanceDataProvider balanceDataProvider =
+    // Provider.of<BalanceDataProvider>(context);
     return Scaffold(
         body: Stack(
       children: [
@@ -49,9 +52,9 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 //text: 'settings'
                 Padding(
                   padding: const EdgeInsets.only(
-                    top: 32.0,
-                    left: 16.0,
-                    right: 16.0,
+                    top: 40.0,
+                    left: 40.0,
+                    right: 40.0,
                     bottom: 0,
                   ),
                   //ListTile for selecting currencies
@@ -74,14 +77,14 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 //line in between
                 Padding(
                   padding: const EdgeInsets.only(
-                    left: 16.0,
-                    right: 16.0,
+                    left: 40.0,
+                    right: 40.0,
                     top: 0,
                     bottom: 16.0,
                   ),
                   child: SizedBox(
                     width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height * 0.003,
+                    height: MediaQuery.of(context).size.height * 0.001,
                     child: const DecoratedBox(
                       decoration: const BoxDecoration(
                         color: Colors.grey,
@@ -90,6 +93,89 @@ class _SettingsScreenState extends State<SettingsScreen> {
                   ),
                 ),
                 //going on...
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 0,
+                    left: 40.0,
+                    right: 40.0,
+                    bottom: 0,
+                  ),
+                  //ListTile for selecting currencies
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Standard Categories'),
+                      ListTile(
+                        // onTap: ontap(),
+                        title: Text('Income'),
+                        trailing: Icon(
+                          Icons.north_east,
+                          color: Colors.black,
+                        ),
+                      ),
+                      ListTile(
+                        // onTap: ontap(),
+                        title: Text('Expenses'),
+                        trailing: Icon(
+                          Icons.south_east,
+                          color: Colors.black,
+                        ),
+                      ),
+                      ListTile(
+                        // onTap: ontap(),
+                        title: Text('Transactions'),
+                        trailing: Icon(
+                          Icons.sync_alt,
+                          color: Colors.black,
+                        ),
+                      ),
+                      //Icons disputable
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 40.0,
+                    right: 40.0,
+                    top: 0,
+                    bottom: 16.0,
+                  ),
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height * 0.001,
+                    child: const DecoratedBox(
+                      decoration: const BoxDecoration(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 0,
+                    left: 40.0,
+                    right: 40.0,
+                    bottom: 0,
+                  ),
+                  //ListTile for selecting currencies
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Special Settings'),
+                        SwitchListTile(
+                          // onTap: ontap(),
+                          title: Text('Schwabenmodus'),
+                          value: _toggled,
+                          onChanged: (_toggled) {
+                            setState(() {
+                              _toggled = true;
+                            });
+                          },
+                        ),
+                      ]),
+                ),
               ],
             ),
           ],
