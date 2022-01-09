d4e90de9bf9026260ccde0ba42e3613938288ddb
Soencke Evers <Soencke.Evers@web.de>
2022-01-10 00:22:59 +0100

    fixed Return type

--- lib/backend_functions/url-handler.dart
+++ lib/backend_functions/url-handler.dart
@@ -1,6 +1,6 @@
 import 'package:url_launcher/url_launcher.dart';
 
-launchURL(String url) async {
+Future<void> launchURL(String url) async {
   if (await canLaunch(url)) {
     await launch(url);
   } else {
