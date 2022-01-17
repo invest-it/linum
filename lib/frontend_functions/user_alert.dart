228b7227be3de65d98fe2b2b084f23b0f994591a
Soencke Evers <Soencke.Evers@web.de>
2022-01-18 00:10:33 +0100

    user alert transformed and ready for action

--- lib/frontend_functions/user_alert.dart
+++ lib/frontend_functions/user_alert.dart
@@ -5,45 +5,44 @@ import 'package:flutter/material.dart';
 
 class UserAlert {
   final BuildContext _context;
-  final String _message;
-  final String _title;
-  final String _actionTitle;
-  final bool _userMustDismissWithButton;
 
-  UserAlert(
-      {message = 'Ein unbekannter Fehler ist aufgetreten.',
-      title = 'Fehler',
-      actionTitle = 'Okay',
-      userMustDismissWithButton = true,
-      required BuildContext context})
-      : _message = message,
-        _title = title,
-        _actionTitle = actionTitle,
-        _userMustDismissWithButton = userMustDismissWithButton,
-        _context = context {
-    _showMyDialog();
+  UserAlert({required BuildContext context}) : _context = context;
+
+  Future<void> showMyDialog2({
+    String message = 'Ein unbekannter Fehler ist aufgetreten.',
+    String title = 'Fehler',
+    String actionTitle = 'Okay',
+    bool userMustDismissWithButton = true,
+  }) {
+    return showMyDialog(message,
+        title: title,
+        actionTitle: actionTitle,
+        userMustDismissWithButton: userMustDismissWithButton);
   }
 
-  static createUserAlert(String message) {}
-
-  Future<void> _showMyDialog() async {
+  Future<void> showMyDialog(
+    String message, {
+    String title = 'Fehler',
+    String actionTitle = 'Okay',
+    bool userMustDismissWithButton = true,
+  }) async {
     return showDialog<void>(
       context: _context,
       barrierDismissible:
-          !_userMustDismissWithButton, // user must tap button if userMustDismissWithButton is true
+          !userMustDismissWithButton, // user must tap button if userMustDismissWithButton is true
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text(_title),
+          title: Text(title),
           content: SingleChildScrollView(
             child: ListBody(
               children: <Widget>[
-                Text(_message),
+                Text(message),
               ],
             ),
           ),
           actions: <Widget>[
             TextButton(
-              child: Text(_actionTitle),
+              child: Text(actionTitle),
               onPressed: () {
                 Navigator.of(context).pop();
               },
