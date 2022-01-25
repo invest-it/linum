f85b80fcdda68453f88ff9fb12f94ed25414630f
Otis Mohr <mail@otismohr.de>
2022-01-26 00:17:30 +0100

    Forgot PW interprets login status, reolves #12

--- lib/widgets/auth/forgot_password.dart
+++ lib/widgets/auth/forgot_password.dart
@@ -15,7 +15,7 @@ class ForgotPasswordButton extends StatelessWidget {
   ForgotPasswordButton(this.providerKey);
 
   final ProviderKey providerKey;
-  final _mailController = TextEditingController();
+  final _inputController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -27,107 +27,217 @@ class ForgotPasswordButton extends StatelessWidget {
     UserAlert userAlert = UserAlert(context: context);
 
     void forgotPWactionLip() {
-      actionLipStatusProvider.setActionLip(
-          providerKey: providerKey,
-          actionLipBody: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                    vertical: 12.0, horizontal: 24.0),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Text(
-                    AppLocalizations.of(context)!.translate(
-                        'action_lip/forgot-password/label-description'),
-                    style: Theme.of(context).textTheme.bodyText1,
-                    textAlign: TextAlign.center,
+      // lip if the user is not logged in
+      if (!authenticationService.isLoggedIn) {
+        actionLipStatusProvider.setActionLip(
+            providerKey: providerKey,
+            actionLipBody: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 12.0, horizontal: 24.0),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text(
+                      AppLocalizations.of(context)!.translate(
+                          'action_lip/forgot-password/logged-out/label-description'),
+                      style: Theme.of(context).textTheme.bodyText1,
+                      textAlign: TextAlign.center,
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                child: Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.all(5.0),
-                      decoration: BoxDecoration(
-                          color: Theme.of(context).colorScheme.onSecondary,
-                          borderRadius: BorderRadius.circular(10),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Theme.of(context).colorScheme.onBackground,
-                              blurRadius: 20.0,
-                              offset: Offset(0, 10),
-                            ),
-                          ]),
-                      child: Column(
-                        children: [
-                          Container(
-                            padding: EdgeInsets.all(8.0),
-                            decoration: BoxDecoration(
-                              border: Border(
-                                bottom: BorderSide(color: Colors.grey.shade100),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(5.0),
+                        decoration: BoxDecoration(
+                            color: Theme.of(context).colorScheme.onSecondary,
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                color:
+                                    Theme.of(context).colorScheme.onBackground,
+                                blurRadius: 20.0,
+                                offset: Offset(0, 10),
+                              ),
+                            ]),
+                        child: Column(
+                          children: [
+                            Container(
+                              padding: EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                border: Border(
+                                  bottom:
+                                      BorderSide(color: Colors.grey.shade100),
+                                ),
+                              ),
+                              child: TextField(
+                                controller: _inputController,
+                                keyboardType: TextInputType.emailAddress,
+                                onEditingComplete: () => {
+                                  authenticationService.resetPassword(
+                                      _inputController.text,
+                                      onError: userAlert.showMyDialog),
+                                },
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: AppLocalizations.of(context)!.translate(
+                                      'onboarding_screen/login-email-hintlabel'),
+                                  hintStyle: Theme.of(context)
+                                      .textTheme
+                                      .bodyText1
+                                      ?.copyWith(
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .secondary),
+                                ),
                               ),
                             ),
-                            child: TextField(
-                              controller: _mailController,
-                              keyboardType: TextInputType.emailAddress,
-                              onEditingComplete: () => {
-                                authenticationService.resetPassword(
-                                    _mailController.text,
-                                    onError: userAlert.showMyDialog),
-                              },
-                              decoration: InputDecoration(
-                                border: InputBorder.none,
-                                hintText: AppLocalizations.of(context)!.translate(
-                                    'onboarding_screen/login-email-hintlabel'),
-                                hintStyle: Theme.of(context)
-                                    .textTheme
-                                    .bodyText1
-                                    ?.copyWith(
-                                        color: Theme.of(context)
-                                            .colorScheme
-                                            .secondary),
-                              ),
-                            ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      height: proportionateScreenHeight(32),
-                    ),
-                    GradientButton(
-                      increaseHeightBy: proportionateScreenHeight(16),
-                      child: Text(
-                        AppLocalizations.of(context)!.translate(
-                            'action_lip/forgot-password/button-submit'),
-                        style: Theme.of(context).textTheme.button,
+                      SizedBox(
+                        height: proportionateScreenHeight(32),
                       ),
-                      callback: () => {
-                        authenticationService.resetPassword(
-                            _mailController.text,
-                            onError: userAlert.showMyDialog),
-                      },
-                      gradient: LinearGradient(
-                        colors: [
-                          Theme.of(context).colorScheme.primary,
-                          createMaterialColor(Color(0xFFC1E695)),
-                        ],
+                      GradientButton(
+                        increaseHeightBy: proportionateScreenHeight(16),
+                        child: Text(
+                          AppLocalizations.of(context)!.translate(
+                              'action_lip/forgot-password/logged-out/button-submit'),
+                          style: Theme.of(context).textTheme.button,
+                        ),
+                        callback: () => {
+                          authenticationService.resetPassword(
+                              _inputController.text,
+                              onError: userAlert.showMyDialog),
+                        },
+                        gradient: LinearGradient(
+                          colors: [
+                            Theme.of(context).colorScheme.primary,
+                            createMaterialColor(Color(0xFFC1E695)),
+                          ],
+                        ),
+                        elevation: 0,
+                        increaseWidthBy: double.infinity,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8)),
                       ),
-                      elevation: 0,
-                      increaseWidthBy: double.infinity,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8)),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            actionLipStatus: ActionLipStatus.ONVIEWPORT,
+            actionLipTitle: AppLocalizations.of(context)!.translate(
+                'action_lip/forgot-password/logged-out/label-title'));
+      }
+      // lip if the user has already authenticated themself
+      else {
+        actionLipStatusProvider.setActionLip(
+            providerKey: providerKey,
+            actionLipBody: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 12.0, horizontal: 24.0),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text(
+                      AppLocalizations.of(context)!.translate(
+                          'action_lip/forgot-password/logged-in/label-description'),
+                      style: Theme.of(context).textTheme.bodyText1,
+                      textAlign: TextAlign.center,
                     ),
-                  ],
+                  ),
                 ),
-              )
-            ],
-          ),
-          actionLipStatus: ActionLipStatus.ONVIEWPORT,
-          actionLipTitle: AppLocalizations.of(context)!
-              .translate('action_lip/forgot-password/label-title'));
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(5.0),
+                        decoration: BoxDecoration(
+                            color: Theme.of(context).colorScheme.onSecondary,
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                color:
+                                    Theme.of(context).colorScheme.onBackground,
+                                blurRadius: 20.0,
+                                offset: Offset(0, 10),
+                              ),
+                            ]),
+                        child: Column(
+                          children: [
+                            Container(
+                              padding: EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                border: Border(
+                                  bottom:
+                                      BorderSide(color: Colors.grey.shade100),
+                                ),
+                              ),
+                              child: TextField(
+                                controller: _inputController,
+                                keyboardType: TextInputType.emailAddress,
+                                onEditingComplete: () => {
+                                  authenticationService.resetPassword(
+                                      _inputController.text,
+                                      onError: userAlert.showMyDialog),
+                                },
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: AppLocalizations.of(context)!.translate(
+                                      'onboarding_screen/login-password-hintlabel'),
+                                  hintStyle: Theme.of(context)
+                                      .textTheme
+                                      .bodyText1
+                                      ?.copyWith(
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .secondary),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: proportionateScreenHeight(32),
+                      ),
+                      GradientButton(
+                        increaseHeightBy: proportionateScreenHeight(16),
+                        child: Text(
+                          AppLocalizations.of(context)!.translate(
+                              'action_lip/forgot-password/logged-in/button-submit'),
+                          style: Theme.of(context).textTheme.button,
+                        ),
+                        callback: () => {
+                          authenticationService
+                              .updatePassword(_inputController.text),
+                        },
+                        gradient: LinearGradient(
+                          colors: [
+                            Theme.of(context).colorScheme.primary,
+                            createMaterialColor(Color(0xFFC1E695)),
+                          ],
+                        ),
+                        elevation: 0,
+                        increaseWidthBy: double.infinity,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8)),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            actionLipStatus: ActionLipStatus.ONVIEWPORT,
+            actionLipTitle: AppLocalizations.of(context)!
+                .translate('action_lip/forgot-password/logged-in/label-title'));
+      }
     }
 
     return OutlinedButton(
