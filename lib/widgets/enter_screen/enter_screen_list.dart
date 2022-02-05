b82a050eeca092d33d5443be899ab95f12f6d127
Otis Mohr <mail@otismohr.de>
2022-02-05 03:22:44 +0100

    Finished Translating Enter Screen, fixed #21

--- lib/widgets/enter_screen/enter_screen_list.dart
+++ lib/widgets/enter_screen/enter_screen_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:linum/backend_functions/local_app_localizations.dart';
 import 'package:linum/models/entry_category.dart';
 import 'package:linum/providers/enter_screen_provider.dart';
 import 'package:linum/widgets/enter_screen/enter_screen_listviewbuilder.dart';
@@ -13,37 +14,68 @@ class EnterScreenList extends StatefulWidget {
 }
 
 class _EnterScreenListState extends State<EnterScreenList> {
-  //all the lists that are displayed in the enter screen
-  final List<EntryCategory> _categoriesExpenses = [
-    EntryCategory(label: "Kategorie", icon: Icons.restaurant),
-    EntryCategory(label: "Datum", icon: Icons.event),
-    EntryCategory(label: "Wiederholen", icon: Icons.loop),
-  ];
-
-  final List<EntryCategory> _categoriesIncome = [
-    EntryCategory(label: "Kategorie", icon: Icons.payments),
-    EntryCategory(label: "Datum", icon: Icons.event),
-    EntryCategory(label: "Wiederholen", icon: Icons.loop),
-  ];
-
-  // final List<EntryCategory> _categoriesTransaction = [
-  //   EntryCategory("Von Account", Icons.local_atm),
-  //   EntryCategory("Zu Account", Icons.local_atm),
-  //   EntryCategory("Datum", Icons.event),
-  //   EntryCategory("Wiederholen", Icons.loop),
-  // ];
-
-  final List<EntryCategory> _categoriesRepeat = [
-    EntryCategory(label: "Täglich", icon: Icons.calendar_today),
-    EntryCategory(label: "Wöchentlich", icon: Icons.next_week),
-    EntryCategory(label: "Monatlich zum 1.", icon: Icons.calendar_view_month),
-    EntryCategory(label: "Frei auswählen", icon: Icons.repeat),
-  ];
-
   @override
   Widget build(BuildContext context) {
     EnterScreenProvider enterScreenProvider =
         Provider.of<EnterScreenProvider>(context);
+
+    //all the lists that are displayed in the enter screen
+    final List<EntryCategory> _categoriesExpenses = [
+      EntryCategory(
+          label: AppLocalizations.of(context)!
+              .translate('enter_screen_attribute_category'),
+          icon: Icons.restaurant),
+      EntryCategory(
+          label: AppLocalizations.of(context)!
+              .translate('enter_screen_attribute_date'),
+          icon: Icons.event),
+      EntryCategory(
+          label: AppLocalizations.of(context)!
+              .translate('enter_screen_attribute_repeat'),
+          icon: Icons.loop),
+    ];
+
+    final List<EntryCategory> _categoriesIncome = [
+      EntryCategory(
+          label: AppLocalizations.of(context)!
+              .translate('enter_screen_attribute_category'),
+          icon: Icons.payments),
+      EntryCategory(
+          label: AppLocalizations.of(context)!
+              .translate('enter_screen_attribute_date'),
+          icon: Icons.event),
+      EntryCategory(
+          label: AppLocalizations.of(context)!
+              .translate('enter_screen_attribute_repeat'),
+          icon: Icons.loop),
+    ];
+
+    // final List<EntryCategory> _categoriesTransaction = [
+    //   EntryCategory("Von Account", Icons.local_atm),
+    //   EntryCategory("Zu Account", Icons.local_atm),
+    //   EntryCategory("Datum", Icons.event),
+    //   EntryCategory("Wiederholen", Icons.loop),
+    // ];
+
+    final List<EntryCategory> _categoriesRepeat = [
+      EntryCategory(
+          label: AppLocalizations.of(context)!
+              .translate('enter_screen/label-repeat-daily'),
+          icon: Icons.calendar_today),
+      EntryCategory(
+          label: AppLocalizations.of(context)!
+              .translate('enter_screen/label-repeat-weekly'),
+          icon: Icons.next_week),
+      EntryCategory(
+          label: AppLocalizations.of(context)!
+              .translate('enter_screen/label-repeat-monthly'),
+          icon: Icons.calendar_view_month),
+      EntryCategory(
+          label: AppLocalizations.of(context)!
+              .translate('enter_screen/label-repeat-freeselect'),
+          icon: Icons.repeat),
+    ];
+
     //returns a different list view builder depending on the page
     if (enterScreenProvider.isExpenses)
       return EnterScreenListViewBuilder(
