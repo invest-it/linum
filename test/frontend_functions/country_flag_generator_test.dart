e399791c94b147d964898156f8efee3c7a4bee4e
Söncke Evers <soencke.evers@web.de>
2022-03-18 12:07:56 +0100

    created first unit test and cleaned up a little bit

---
+++ test/frontend_functions/country_flag_generator_test.dart
@@ -0,0 +1,63 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:linum/frontend_functions/country_flag_generator.dart';
+
+void main() {
+  group("frontend functions", () {
+    group("country_flag_generator", () {
+      test("gives correct german flag", () {
+        // Arrange (Initialization)
+        String exprectedFlag = "🇩🇪";
+
+        // Act (Execution)
+        String flag = countryFlag("de");
+
+        // Assert (Observation)
+        expect(flag, exprectedFlag);
+      });
+
+      test("gives correct great britain flag", () {
+        // Arrange (Initialization)
+        String exprectedFlag = "🇬🇧";
+
+        // Act (Execution)
+        String flag = countryFlag("gb");
+
+        // Assert (Observation)
+        expect(flag, exprectedFlag);
+      });
+
+      test("gives correct flag of the Netherlands", () {
+        // Arrange (Initialization)
+        String exprectedFlag = "🇳🇱";
+
+        // Act (Execution)
+        String flag = countryFlag("nl");
+
+        // Assert (Observation)
+        expect(flag, exprectedFlag);
+      });
+
+      test("gives correct spain flag", () {
+        // Arrange (Initialization)
+        String exprectedFlag = "🇪🇸";
+
+        // Act (Execution)
+        String flag = countryFlag("es");
+
+        // Assert (Observation)
+        expect(flag, exprectedFlag);
+      });
+
+      test("gives correct french flag", () {
+        // Arrange (Initialization)
+        String exprectedFlag = "🇫🇷";
+
+        // Act (Execution)
+        String flag = countryFlag("fr");
+
+        // Assert (Observation)
+        expect(flag, exprectedFlag);
+      });
+    });
+  });
+}
