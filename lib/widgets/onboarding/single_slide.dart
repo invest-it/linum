bcd1b3203b0a467f7aebdf4953c5fbc718fb263c
Martin Mayer <ms-mayer@outlook.de>
2022-05-29 00:00:59 +0200

    Removed unused imports

--- lib/widgets/onboarding/single_slide.dart
+++ lib/widgets/onboarding/single_slide.dart
@@ -3,9 +3,6 @@
 //  Author: NightmindOfficial
 //  Co-Author: n/a
 //  (Refactored by damattl)
-
-import 'dart:developer';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
