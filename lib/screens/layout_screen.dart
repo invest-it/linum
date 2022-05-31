51fce9e953435a802b2dd4531eb0e68cbc5d4f45
Marc Nickel <marc@zubik.io>
2022-05-31 20:43:22 +0200

    Fixing again

--- lib/screens/layout_screen.dart
+++ lib/screens/layout_screen.dart
@@ -33,12 +33,12 @@ class _LayoutScreenState extends State<LayoutScreen>
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance?.addObserver(this);
+    WidgetsBinding.instance.addObserver(this);
   }
 
   @override
   void dispose() {
-    WidgetsBinding.instance?.removeObserver(this);
+    WidgetsBinding.instance.removeObserver(this);
     super.dispose();
   }
 
