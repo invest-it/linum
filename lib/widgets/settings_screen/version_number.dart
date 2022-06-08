78ba47aaac076c06a20e703eca80cc2dc05c141c
Marc Nickel <marc@zubik.io>
2022-06-08 19:07:51 +0200

    Extracted Version Number #207

---
+++ lib/widgets/settings_screen/version_number.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+
+class VersionNumber extends StatefulWidget {
+  @override
+  State<VersionNumber> createState() => _VersionNumberState();
+}
+
+class _VersionNumberState extends State<VersionNumber> {
+  PackageInfo _packageInfo = PackageInfo(
+    appName: 'Unknown',
+    packageName: 'Unknown',
+    version: 'Unknown',
+    buildNumber: 'Unknown',
+    buildSignature: 'Unknown',
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _initPackageInfo();
+  }
+
+  Future<void> _initPackageInfo() async {
+    final info = await PackageInfo.fromPlatform();
+    setState(() {
+      _packageInfo = info;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Text(
+        '${_packageInfo.appName} Version ${_packageInfo.version} (${_packageInfo.buildNumber})',
+        style: Theme.of(context).textTheme.bodyText2?.copyWith(
+              color: Theme.of(context).colorScheme.tertiaryContainer,
+              letterSpacing: 0,
+            ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
