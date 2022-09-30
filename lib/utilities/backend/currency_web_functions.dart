725345d2601ffda8a25c510ff16a66e97b4de792
Martin Mayer <ms-mayer@outlook.de>
2022-09-30 09:58:56 +0200

    feat: fetch exchange rate for date and currency

--- lib/utilities/backend/currency_web_functions.dart
+++ lib/utilities/backend/currency_web_functions.dart
@@ -1,1 +1,22 @@
+import 'dart:convert';
 
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+import 'package:linum/models/exchange_rate.dart';
+
+const webAPIUrl = "https://exchange-rates.linum.martins-lightart.de/";
+
+Future<ExchangeRate> fetchExchangeRateForDate(DateTime date, String currency) async {
+  final dateStr = DateFormat("yyyy-MM-dd").format(date);
+  final response = await http
+      .get(Uri.parse("$webAPIUrl/rate/$currency/$dateStr"));
+
+  if (response.statusCode == 200) {
+    final jsonMap = jsonDecode(response.body) as Map<String, dynamic>;
+    // TODO: Handle errors
+    return ExchangeRate.fromJson(jsonMap);
+  } else {
+    throw Exception("Failed to get Exchange Rate");
+    // TODO: Handle error more elegantly. Use API Error Codes
+  }
+}
