0b91ffb500b76cc57a21c621b7db42da8deb2819
Martin Mayer <ms-mayer@outlook.de>
2022-09-30 10:02:49 +0200

    feat: fetch supported currencies list

--- lib/utilities/backend/currency_web_functions.dart
+++ lib/utilities/backend/currency_web_functions.dart
@@ -8,8 +8,8 @@ const webAPIUrl = "https://exchange-rates.linum.martins-lightart.de/";
 
 Future<ExchangeRate> fetchExchangeRateForDate(DateTime date, String currency) async {
   final dateStr = DateFormat("yyyy-MM-dd").format(date);
-  final response = await http
-      .get(Uri.parse("$webAPIUrl/rate/$currency/$dateStr"));
+  final uri = Uri.parse("$webAPIUrl/rate/$currency/$dateStr");
+  final response = await http.get(uri);
 
   if (response.statusCode == 200) {
     final jsonMap = jsonDecode(response.body) as Map<String, dynamic>;
@@ -17,6 +17,20 @@ Future<ExchangeRate> fetchExchangeRateForDate(DateTime date, String currency) as
     return ExchangeRate.fromJson(jsonMap);
   } else {
     throw Exception("Failed to get Exchange Rate");
+    // TODO: Handle error more elegantly. Use API Error Codes
+  }
+}
+
+Future<List<String>> fetchSupportedCurrencies() async {
+  final uri = Uri.parse("$webAPIUrl/supported");
+  final response = await http.get(uri);
+
+  if (response.statusCode == 200) {
+    final jsonList = jsonDecode(response.body) as List<String>;
+    // TODO: Handle errors
+    return jsonList;
+  } else {
+    throw Exception("Failed to get supported currencies");
     // TODO: Handle error more elegantly. Use API Error Codes
   }
 }
