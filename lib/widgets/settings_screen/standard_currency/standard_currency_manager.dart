069ce426fe59b45aa9a40b0fc8f9c12b28a97812
TheBlueBaron <shadowxd02@gmail.com>
2022-11-06 22:05:47 +0100

    Deleted author

--- lib/widgets/settings_screen/standard_currency/standard_currency_manager.dart
+++ lib/widgets/settings_screen/standard_currency/standard_currency_manager.dart
@@ -1,17 +1,7 @@
-//  Settings Screen Standard Category - The selector for the standard categories
-//
-//  Author: aronzimmermann
-//  Co-Author: SoTBurst, NightmindOfficial
-//  Refactored: TheBlueBaron
-
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:linum/constants/settings_enums.dart';
 import 'package:linum/providers/account_settings_provider.dart';
 import 'package:linum/providers/action_lip_status_provider.dart';
 import 'package:linum/widgets/screen_skeleton/screen_skeleton.dart';
-import 'package:linum/widgets/settings_screen/standard_category/category_list_tile.dart';
-import 'package:linum/widgets/settings_screen/standard_category/category_list_view.dart';
 import 'package:linum/widgets/settings_screen/standard_currency/currency_list_tile.dart';
 import 'package:linum/widgets/settings_screen/standard_currency/curreny_list_view.dart';
 import 'package:provider/provider.dart';
