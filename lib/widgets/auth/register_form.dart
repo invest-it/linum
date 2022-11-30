e0778ba68d56f632ecbcce51ad27f52a345b847c
Otis Mohr <mail@otismohr.de>
2022-11-30 18:47:01 +0100

    fix: Privacy Policy Link

--- lib/widgets/auth/register_form.dart
+++ lib/widgets/auth/register_form.dart
@@ -250,7 +250,7 @@ class _RegisterFormState extends State<RegisterForm> {
                       recognizer: TapGestureRecognizer()
                         ..onTap = () async {
                           launchURL(
-                            'https://investit.notion.site/Datenschutzerkl-rung-Linum-Budgeting-App-b3da92953a224afba4336450a92b5d90',
+                            'https://privacy.linum-app.de',
                           );
                         },
                       style: Theme.of(context).textTheme.bodyText2?.copyWith(
