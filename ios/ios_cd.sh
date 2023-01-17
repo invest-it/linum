0922602200dcbafa7b8cb856a0b868b6239b852e
Martin Mayer <ms-mayer@outlook.de>
2023-01-17 21:07:07 +0100

    fix: sh variable whitespaces

--- ios/ios_cd.sh
+++ ios/ios_cd.sh
@@ -5,7 +5,7 @@ cd ./ios
 gem install bundler
 bundle install
 
-GIT_BASIC_AUTH_TOKEN = $($GIT_USER_NAME:$GIT_ACCESS_TOKEN | base64)
+GIT_BASIC_AUTH_TOKEN=$($GIT_USER_NAME:$GIT_ACCESS_TOKEN | base64)
 echo (echo "$APP_STORE_AUTH_KEY" | base64 --decode) > AuthKey.p8
 
 cd ../
