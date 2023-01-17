4a42ded511eded8ed1b3cfcec0e2bc25a87840c9
Martin Mayer <ms-mayer@outlook.de>
2023-01-17 21:30:48 +0100

    feat: add android cd script

---
+++ android/android_cd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd ./android
+
+gem install bundler
+bundle install
+
+echo (echo "$UPLOAD_KEYSTORE_JKS" | base64 --decode) > key.jks
+echo (echo "$KEY_PROPERTIES" | base64 --decode) > key.properties
+
+cd ../
+flutter build appbundle
+
+cd ./android
+bundle exec fastlane beta
