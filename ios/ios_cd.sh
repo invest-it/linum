ead5516de4d11b2b6d0d1a6590661cb8823a5c6d
Martin Mayer <ms-mayer@outlook.de>
2023-01-17 22:07:44 +0100

    fix: typos

--- ios/ios_cd.sh
+++ ios/ios_cd.sh
@@ -8,7 +8,7 @@ bundle install
 GIT_USER_NAME = damattl
 GIT_ACCESS_TOKEN = Test
 
-GIT_BASIC_AUTH_TOKEN=$(echo "$GITS_USER_NAME":"$GIT_ACCESS_TOKEN" | base64)
+GIT_BASIC_AUTH_TOKEN=$(echo "$GIT_USER_NAME":"$GIT_ACCESS_TOKEN" | base64)
 echo (echo "$APP_STORE_AUTH_KEY" | base64 --decode) > AuthKey.p8
 
 cd ../
