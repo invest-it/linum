f1a0a713462a9c033e23ca1462537bb7fb544fd3
Martin Mayer <ms-mayer@outlook.de>
2023-01-17 21:46:56 +0100

    fix: git auth generation

--- ios/ios_cd.sh
+++ ios/ios_cd.sh
@@ -5,7 +5,10 @@ cd ./ios
 gem install bundler
 bundle install
 
-GIT_BASIC_AUTH_TOKEN=$($GIT_USER_NAME:$GIT_ACCESS_TOKEN | base64)
+GIT_USER_NAME = damattl
+GIT_ACCESS_TOKEN = Test
+
+GIT_BASIC_AUTH_TOKEN=$(echo "$GITS_USER_NAME":"$GIT_ACCESS_TOKEN" | base64)
 echo (echo "$APP_STORE_AUTH_KEY" | base64 --decode) > AuthKey.p8
 
 cd ../
