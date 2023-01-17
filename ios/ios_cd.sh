3de4d9119fc3a9ab1d5ec82d6e72df30f3f6e6a1
Martin Mayer <ms-mayer@outlook.de>
2023-01-17 22:16:41 +0100

    fix: more typos

--- ios/ios_cd.sh
+++ ios/ios_cd.sh
@@ -2,11 +2,11 @@
 
 cd ./ios
 
-gem install bundler
+gem install bundler:1.17.2
 bundle install
 
 GIT_BASIC_AUTH_TOKEN=$(echo "$GIT_USER_NAME":"$GIT_ACCESS_TOKEN" | base64)
-echo (echo "$APP_STORE_AUTH_KEY" | base64 --decode) > AuthKey.p8
+echo $(echo "$APP_STORE_AUTH_KEY" | base64 --decode) > AuthKey.p8
 
 cd ../
 flutter build ios --release --no-codesign
