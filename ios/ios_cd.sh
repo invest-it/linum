33d23628c499f466bec7cc9704399e93f207a60e
Martin Mayer <ms-mayer@outlook.de>
2023-01-18 13:12:17 +0100

    fix: added .encrypted

--- ios/ios_cd.sh
+++ ios/ios_cd.sh
@@ -6,7 +6,7 @@ gem install bundler:1.17.2
 bundle install
 
 export GIT_BASIC_AUTH_TOKEN=$(echo -n "$GIT_USER_NAME:$GIT_ACCESS_TOKEN" | base64 | tr -d \\n)
-echo $(echo "$APP_STORE_AUTH_KEY" | base64 --decode) > AuthKey.p8
+openssl aes-256-cbc -d -in .encrypted -k $AUTH_KEY_ENCRYPTION_KEY >> ./AuthKey.p8
 
 cd ../
 flutter build ios --release --no-codesign
