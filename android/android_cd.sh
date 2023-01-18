558c255f3edef12112edeaf81f6c09485aa776c6
Martin Mayer <ms-mayer@outlook.de>
2023-01-18 10:53:54 +0100

    fix: setup ruby on android aswell

--- android/android_cd.sh
+++ android/android_cd.sh
@@ -5,7 +5,9 @@ cd ./android
 gem install bundler:1.17.2
 bundle install
 
-echo $(echo "$UPLOAD_KEYSTORE_JKS" | base64 --decode) > ./app/upload_key.jks
+ls
+
+echo $(echo "$UPLOAD_KEYSTORE_JKS" | base64 --decode) > ./app/upload_keystore.jks
 echo $(echo "$KEY_PROPERTIES" | base64 --decode) > key.properties
 
 cd ../
