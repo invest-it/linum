660244038104cf454bc647e3fd4903ef271ec443
Martin Mayer <ms-mayer@outlook.de>
2023-01-18 16:14:51 +0100

    fix: die hoffnung stirbt zuletzt, aber sie stirbt

--- ios/fastlane/README.md
+++ ios/fastlane/README.md
@@ -23,6 +23,14 @@ For _fastlane_ installation instructions, see [Installing _fastlane_](https://do
 
 
 
+### ios localBeta
+
+```sh
+[bundle exec] fastlane ios localBeta
+```
+
+
+
 ### ios beta
 
 ```sh
--- ios/ios_cd.sh
+++ ios/ios_cd.sh
@@ -8,7 +8,6 @@ bundle install
 export GIT_BASIC_AUTH_TOKEN=$(echo -n "$GIT_USER_NAME:$GIT_ACCESS_TOKEN" | base64 | tr -d \\n)
 openssl aes-256-cbc -d -in .encrypted -k $AUTH_KEY_ENCRYPTION_KEY >> ./AuthKey.p8
 
-cd ../
 flutter build ios --release --no-codesign
 
 export GIT_TERMINAL_PROMPT=1
