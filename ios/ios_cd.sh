6e940550112fad4358a3cb411d47f432dfaa350e
Martin Mayer <ms-mayer@outlook.de>
2023-01-18 12:44:18 +0100

    fix: more errors

--- ios/ios_cd.sh
+++ ios/ios_cd.sh
@@ -5,11 +5,14 @@ cd ./ios
 gem install bundler:1.17.2
 bundle install
 
-GIT_BASIC_AUTH_TOKEN=$(echo -n "$GIT_USER_NAME:$GIT_ACCESS_TOKEN" | base64 | tr -d \\n)
+export GIT_BASIC_AUTH_TOKEN=$(echo -n "$GIT_USER_NAME:$GIT_ACCESS_TOKEN" | base64 | tr -d \\n)
 echo $(echo "$APP_STORE_AUTH_KEY" | base64 --decode) > AuthKey.p8
 
 cd ../
 flutter build ios --release --no-codesign
+
+echo GIT_TERMINAL_PROMPT
+export GIT_TERMINAL_PROMPT=1
 
 cd ./ios
 bundle exec fastlane beta
