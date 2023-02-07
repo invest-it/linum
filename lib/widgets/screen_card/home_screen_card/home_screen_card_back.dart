eb2609c355aeaae2c7f3eb0cba360d351dc32fbe
NightmindOfficial <mail@otismohr.de>
2023-02-08 00:49:02 +0100

    Initial structural changes to HSC Back

--- lib/widgets/screen_card/home_screen_card/home_screen_card_back.dart
+++ lib/widgets/screen_card/home_screen_card/home_screen_card_back.dart
@@ -11,10 +11,10 @@ import 'package:linum/providers/account_settings_provider.dart';
 import 'package:linum/providers/algorithm_provider.dart';
 import 'package:linum/providers/balance_data_provider.dart';
 import 'package:linum/providers/screen_card_provider.dart';
-import 'package:linum/utilities/frontend/currency_formatter.dart';
 import 'package:linum/utilities/frontend/homescreen_card_time_warp.dart';
 import 'package:linum/widgets/loading_spinner.dart';
 import 'package:linum/widgets/screen_card/card_widgets/home_screen_card_avatar.dart';
+import 'package:linum/widgets/screen_card/home_screen_card/home_screen_card_overline_header_row.dart';
 import 'package:linum/widgets/screen_card/home_screen_card/home_screen_card_row.dart';
 import 'package:linum/widgets/screen_card/home_screen_functions.dart';
 import 'package:linum/widgets/screen_card/screen_card_data_extensions.dart';
@@ -41,42 +41,32 @@ class HomeScreenCardBack extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Column(
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Spacer(),
-                  Center(
-                    child: Text(
-                      'Monatsplanung', //TODO @NightmindOfficial translate!
-                      style: MediaQuery.of(context).size.height < 650
-                          ? Theme.of(context).textTheme.headline5
-                          : Theme.of(context).textTheme.headline4,
-                    ),
-                  ),
-                  Expanded(
-                    child: Align(
-                      alignment: Alignment.topRight,
-                      child: IconButton(
-                        padding: const EdgeInsets.only(right: 16),
-                        constraints: const BoxConstraints(),
-                        onPressed: () {
-                          screenCardProvider.controller?.toggleCard();
-                        },
-                        icon: const Icon(
-                          Icons.flip_camera_android_rounded,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Text(
-                dateFormat.format(
-                  algorithmProvider.currentShownMonth,
+              const Spacer(),
+              Center(
+                child: Text(
+                  "Monatsplanung | Feb '23", //TODO @NightmindOfficial translate!
+                  style: MediaQuery.of(context).size.height < 650
+                      ? Theme.of(context).textTheme.headline5
+                      : Theme.of(context).textTheme.headline4,
                 ),
-                style: Theme.of(context).textTheme.bodyText1,
+              ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: IconButton(
+                    padding: const EdgeInsets.only(right: 16),
+                    constraints: const BoxConstraints(),
+                    onPressed: () {
+                      screenCardProvider.controller?.toggleCard();
+                    },
+                    icon: const Icon(
+                      Icons.flip_camera_android_rounded,
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
@@ -90,39 +80,44 @@ class HomeScreenCardBack extends StatelessWidget {
                 icon: const Icon(Icons.arrow_back_ios_new_rounded),
               ),
               Expanded(
-                child: StreamBuilder<HomeScreenCardData>(
-                  stream: balanceDataProvider.getHomeScreenCardData(),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.none ||
-                        snapshot.connectionState == ConnectionState.waiting) {
-                      return const LoadingSpinner();
-                    }
-                    return Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.baseline,
-                      textBaseline: TextBaseline.alphabetic,
-                      children: CurrencyFormatter(
-                        context.locale,
-                        symbol: settings.getStandardCurrency().symbol,
-                      ).formatWithWidgets(
-                        snapshot.data?.eomBalance ?? 0,
-                        (amount) => Flexible(
-                          child: FittedBox(
-                            fit: BoxFit.scaleDown,
-                            child: Text(
-                              amount,
-                              style: getBalanceTextStyle(
-                                  context, snapshot.data?.eomBalance ?? 0),
-                            ),
-                          ),
-                        ),
-                        (symbol) => Text(
-                          symbol,
-                          style: Theme.of(context).textTheme.bodyText1,
-                        ),
-                      ),
-                    );
-                  },
+                child: Column(
+                  children: [
+                    //UPPER OVERLINE HEADER HERE
+                    const HomeScreenCardOverlineHeaderRow([
+                      "Bilanz",
+                      "± offene Verträge",
+                      "SHDIAHSD",
+                      "HAUHDSUD",
+                      "HAUSDHASUHDASHDAS"
+                    ]),
+                    StreamBuilder<HomeScreenCardData>(
+                      stream: balanceDataProvider.getHomeScreenCardData(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.none ||
+                            snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                          return const LoadingSpinner();
+                        }
+                        return Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.baseline,
+                          textBaseline: TextBaseline.alphabetic,
+                          children: [
+                            const Text("EOM"),
+                            Column(
+                              children: const [
+                                Text("Plus"),
+                                Text("Minus"),
+                              ],
+                            )
+                          ],
+                        );
+                      },
+                    ),
+                    //LOWER OVERLINE HEADER HERE
+                    const HomeScreenCardOverlineHeaderRow(
+                        ["= aktueller Kontostand"]),
+                  ],
                 ),
               ),
               IconButton(
@@ -152,3 +147,29 @@ class HomeScreenCardBack extends StatelessWidget {
     );
   }
 }
+
+
+
+// CURRENCY FORMATTER TEMPLATE 
+
+
+// CurrencyFormatter(
+//                         context.locale,
+//                         symbol: settings.getStandardCurrency().symbol,
+//                       ).formatWithWidgets(
+//                         snapshot.data?.eomBalance ?? 0,
+//                         (amount) => Flexible(
+//                           child: FittedBox(
+//                             fit: BoxFit.scaleDown,
+//                             child: Text(
+//                               amount,
+//                               style: getBalanceTextStyle(
+//                                   context, snapshot.data?.eomBalance ?? 0),
+//                             ),
+//                           ),
+//                         ),
+//                         (symbol) => Text(
+//                           symbol,
+//                           style: Theme.of(context).textTheme.bodyText1,
+//                         ),
+//                       ),
