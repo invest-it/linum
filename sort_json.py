146de2b59e443f7190739f720c52fba09f05d4ca
Martin Mayer <ms-mayer@outlook.de>
2023-02-25 23:30:03 +0100

    fix: sorted lang files

--- sort_json.py
+++ sort_json.py
@@ -5,9 +5,9 @@ filename = sys.argv[1]
 if filename == "" or None:
    exit()
 
-with open(filename, 'r') as f:
+with open(filename, 'r', encoding='utf-8') as f:
   data = json.load(f)
 
 
-with open(filename, "w") as jsonFile:
-    json.dump(data, jsonFile, sort_keys=True, indent=4)
+with open(filename, "w", encoding='utf-8') as jsonFile:
+    json.dump(data, jsonFile, sort_keys=True, ensure_ascii=False, indent=4)
