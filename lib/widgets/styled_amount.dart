b8d5742dfb61b8d29fd1b1f3e88d2a5af122974a
NightmindOfficial <mail@otismohr.de>
2023-02-26 01:44:28 +0100

    Standardize Theming and Alignment of StyledAmount Negative Values are now red by default

--- lib/widgets/styled_amount.dart
+++ lib/widgets/styled_amount.dart
@@ -5,6 +5,7 @@ class StyledAmount extends StatelessWidget {
   final String symbol;
   final TextAlign txtAlign;
   late final bool _euroMode;
+  late final bool _isNegative;
   late final String _formatted;
 
   StyledAmount(
@@ -15,30 +16,46 @@ class StyledAmount extends StatelessWidget {
   }) {
     _formatted = CurrencyFormatter(locale, symbol: symbol).format(value);
     _euroMode = CurrencyFormatter(locale, symbol: symbol).amountBeforeSymbol();
+    _isNegative = value < 0;
   }
 
   @override
   Widget build(BuildContext context) {
     final RegExp exp = RegExp('(-?[0-9]+)([,.](?:[0-9]+))');
     final expMatch = exp.firstMatch(_formatted);
+    final _regularTheme3 = Theme.of(context).textTheme.headline3;
+    final _regularTheme4 = Theme.of(context).textTheme.headline4;
+    final _errorTheme3 = Theme.of(context)
+        .textTheme
+        .headline3
+        ?.copyWith(color: Theme.of(context).colorScheme.error);
+    final _errorTheme4 = Theme.of(context)
+        .textTheme
+        .headline4
+        ?.copyWith(color: Theme.of(context).colorScheme.error);
 
     if (_euroMode) {
       //Euro Case
-      return RichText(
-        textAlign: txtAlign,
-        maxLines: 1,
-        text: TextSpan(
-          children: [
-            TextSpan(
+      return Align(
+        child: RichText(
+          textAlign: txtAlign,
+          maxLines: 1,
+          text: TextSpan(
+            children: [
+              TextSpan(
                 text: expMatch?.group(1) ?? "??",
-                style: Theme.of(context).textTheme.headline4),
-            TextSpan(
+                style: _isNegative ? _errorTheme3 : _regularTheme3,
+              ),
+              TextSpan(
                 text: expMatch?.group(2) ?? ".??",
-                style: Theme.of(context).textTheme.headline5),
-            TextSpan(
-                text: symbol, style: Theme.of(context).textTheme.headline4),
-          ],
-          style: Theme.of(context).textTheme.bodyText2,
+                style: _isNegative ? _errorTheme4 : _regularTheme4,
+              ),
+              TextSpan(
+                text: symbol,
+                style: _isNegative ? _errorTheme3 : _regularTheme3,
+              ),
+            ],
+          ),
         ),
       );
     } else {
@@ -49,15 +66,18 @@ class StyledAmount extends StatelessWidget {
         text: TextSpan(
           children: [
             TextSpan(
-                text: symbol, style: Theme.of(context).textTheme.headline4),
+              text: symbol,
+              style: _isNegative ? _errorTheme3 : _regularTheme3,
+            ),
             TextSpan(
-                text: expMatch?.group(1) ?? "??",
-                style: Theme.of(context).textTheme.headline4),
+              text: expMatch?.group(1) ?? "??",
+              style: _isNegative ? _errorTheme3 : _regularTheme3,
+            ),
             TextSpan(
-                text: expMatch?.group(2) ?? ".??",
-                style: Theme.of(context).textTheme.headline5),
+              text: expMatch?.group(2) ?? ".??",
+              style: _isNegative ? _errorTheme4 : _regularTheme4,
+            ),
           ],
-          style: Theme.of(context).textTheme.bodyText2,
         ),
       );
     }
