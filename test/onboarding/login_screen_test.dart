46828e5476ba6a5cca4632194b43ade07a4a2b82
Soencke Evers <Soencke.Evers@web.de>
2023-03-01 02:53:30 +0100

    test: created the first functioning test for login screen

--- test/onboarding/login_screen_test.dart
+++ test/onboarding/login_screen_test.dart
@@ -1,74 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:linum/providers/action_lip_status_provider.dart';
+import 'package:linum/providers/authentication_service.dart';
 import 'package:linum/providers/onboarding_screen_provider.dart';
+import 'package:linum/providers/size_guide_provider.dart';
 import 'package:linum/widgets/auth/login_form.dart';
 import 'package:linum/widgets/onboarding/login_screen.dart';
+import 'package:linum/widgets/screen_skeleton/screen_skeleton.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:provider/provider.dart';
 
-// Annotation which generates the cat.mocks.dart library and the MockCat class.
-@GenerateNiceMocks([MockSpec<BuildContext>()])
-@GenerateNiceMocks([MockSpec<DiagnosticsNode>()])
-@GenerateNiceMocks([MockSpec<MediaQueryData>()])
-@GenerateNiceMocks([MockSpec<MediaQuery>()])
 @GenerateNiceMocks([MockSpec<OnboardingScreenProvider>()])
+@GenerateNiceMocks([MockSpec<SizeGuideProvider>()])
+@GenerateNiceMocks([MockSpec<AuthenticationService>()])
 import 'login_screen_test.mocks.dart';
 
 void main() {
   group('LoginScreen', () {
-    late MockOnboardingScreenProvider mockProvider;
-    late MockBuildContext mockBuildContext;
-    late MockMediaQuery mockMediaQuery;
-    late MockMediaQueryData mockMediaQueryData;
-    late MockDiagnosticsNode mockDiagnosticsNode;
-
-    const screenHeight = 812.0;
-    const screenWidth = 375.0;
-
+    late MockOnboardingScreenProvider mockOnboardingScreenProvider;
+    late MockSizeGuideProvider mockSizeGuideProvider;
+    late MockAuthenticationService mockAuthenticationService;
     setUp(() {
-      mockProvider = MockOnboardingScreenProvider();
-      when(mockProvider.pageState).thenReturn(OnboardingPageState.none);
+      mockOnboardingScreenProvider = MockOnboardingScreenProvider();
+      when(mockOnboardingScreenProvider.pageState)
+          .thenReturn(OnboardingPageState.none);
 
-      mockMediaQueryData = MockMediaQueryData();
-      when(mockMediaQueryData.size)
-          .thenReturn(const Size(screenWidth, screenHeight));
-      when(mockMediaQueryData.orientation).thenReturn(Orientation.portrait);
-      when(mockMediaQueryData.viewInsets)
-          .thenReturn(const EdgeInsets.fromLTRB(0, 0, 0, 100));
+      mockSizeGuideProvider = MockSizeGuideProvider();
 
-      mockMediaQuery = MockMediaQuery();
-      when(mockMediaQuery.data).thenReturn(mockMediaQueryData);
-
-      mockDiagnosticsNode = MockDiagnosticsNode();
-
-      mockBuildContext = MockBuildContext();
-      when(mockBuildContext.dependOnInheritedWidgetOfExactType<MediaQuery>())
-          .thenReturn(mockMediaQuery);
-      when(mockBuildContext.describeWidget(any))
-          .thenReturn(mockDiagnosticsNode);
-      when(mockBuildContext.describeOwnershipChain(any))
-          .thenReturn(mockDiagnosticsNode);
-
-      // SizeGuide.init(mockBuildContext);
+      mockAuthenticationService = MockAuthenticationService();
     });
 
-    testWidgets('renders correctly', (WidgetTester tester) async {
+    testWidgets('renders all children', (WidgetTester tester) async {
       await tester.pumpWidget(
         MaterialApp(
-          home: ChangeNotifierProvider.value(
-            value: mockProvider,
-            child: const LoginScreen(),
+          home: Scaffold(
+            body: MultiProvider(
+              providers: [
+                ChangeNotifierProvider<OnboardingScreenProvider>(
+                  create: (_) => mockOnboardingScreenProvider,
+                ),
+                ChangeNotifierProvider<SizeGuideProvider>(
+                  create: (_) => mockSizeGuideProvider,
+                ),
+                ChangeNotifierProvider<AuthenticationService>(
+                  create: (_) => mockAuthenticationService,
+                ),
+                ChangeNotifierProvider<ActionLipStatusProvider>(
+                  create: (_) => ActionLipStatusProvider(),
+                ),
+              ],
+              child: const LoginScreen(),
+            ),
           ),
         ),
       );
 
-      expect(find.byType(GestureDetector), findsOneWidget);
-      expect(find.byType(AnimatedContainer), findsOneWidget);
-      expect(find.byType(Column), findsOneWidget);
+      expect(find.byType(GestureDetector), findsWidgets);
+      expect(find.byType(AnimatedContainer), findsWidgets);
+      expect(find.byType(Column), findsWidgets);
+      expect(find.byType(Row), findsWidgets);
       expect(find.byType(LoginForm), findsOneWidget);
       expect(find.byType(Positioned), findsOneWidget);
       expect(find.byType(ClipRRect), findsNWidgets(2));
+      expect(find.byType(TextField), findsNWidgets(2));
     });
   });
 }
