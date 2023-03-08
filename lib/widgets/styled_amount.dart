8da9c7f2b9027b638e0ea8521b90e4cfa7f24321
NightmindOfficial <mail@otismohr.de>
2023-03-08 01:12:15 +0100

    Styled Amount now has different sizing modes!

--- lib/widgets/styled_amount.dart
+++ lib/widgets/styled_amount.dart
@@ -4,35 +4,75 @@ import 'package:linum/utilities/frontend/currency_formatter.dart';
 class StyledAmount extends StatelessWidget {
   final String symbol;
   final TextAlign txtAlign;
+  final StyledFontSize fontSize;
   late final bool _euroMode;
   late final bool _isNegative;
+  late final num value;
   late final String _formatted;
 
   StyledAmount(
-    num value,
+    this.value,
     Locale locale,
     this.symbol, {
+    this.fontSize = StyledFontSize.standard,
     this.txtAlign = TextAlign.center,
+    bool alwaysNegative = false,
   }) {
     _formatted = CurrencyFormatter(locale, symbol: symbol).format(value);
     _euroMode = CurrencyFormatter(locale, symbol: symbol).amountBeforeSymbol();
-    _isNegative = value < 0;
+    _isNegative = alwaysNegative || value < 0;
   }
 
   @override
   Widget build(BuildContext context) {
     final RegExp exp = RegExp('(-?[0-9]+)([,.](?:[0-9]+))');
     final expMatch = exp.firstMatch(_formatted);
-    final _regularTheme3 = Theme.of(context).textTheme.headline3;
-    final _regularTheme4 = Theme.of(context).textTheme.headline4;
-    final _errorTheme3 = Theme.of(context)
-        .textTheme
-        .headline3
-        ?.copyWith(color: Theme.of(context).colorScheme.error);
-    final _errorTheme4 = Theme.of(context)
-        .textTheme
-        .headline4
-        ?.copyWith(color: Theme.of(context).colorScheme.error);
+
+    TextStyle? normalFontTheme;
+    TextStyle? smallFontTheme;
+    TextStyle? normalFontThemeNeg;
+    TextStyle? smallFontThemeNeg;
+
+    switch (fontSize) {
+      case StyledFontSize.standard:
+        normalFontTheme = Theme.of(context).textTheme.headline3;
+        smallFontTheme = Theme.of(context).textTheme.headline4;
+        normalFontThemeNeg = Theme.of(context)
+            .textTheme
+            .headline3
+            ?.copyWith(color: Theme.of(context).colorScheme.error);
+        smallFontThemeNeg = Theme.of(context)
+            .textTheme
+            .headline4
+            ?.copyWith(color: Theme.of(context).colorScheme.error);
+        break;
+      case StyledFontSize.compact:
+        normalFontTheme = Theme.of(context).textTheme.headline4;
+        smallFontTheme = Theme.of(context).textTheme.headline5;
+        normalFontThemeNeg = Theme.of(context)
+            .textTheme
+            .headline4
+            ?.copyWith(color: Theme.of(context).colorScheme.error);
+        smallFontThemeNeg = Theme.of(context)
+            .textTheme
+            .headline5
+            ?.copyWith(color: Theme.of(context).colorScheme.error);
+        break;
+      case StyledFontSize.maximize:
+        normalFontTheme = Theme.of(context).textTheme.headline2?.copyWith(
+              fontSize: 39.81,
+            );
+        smallFontTheme = Theme.of(context).textTheme.headline2;
+        normalFontThemeNeg = Theme.of(context).textTheme.headline2?.copyWith(
+              color: Theme.of(context).colorScheme.error,
+              fontSize: 39.81,
+            );
+        smallFontThemeNeg = Theme.of(context)
+            .textTheme
+            .headline2
+            ?.copyWith(color: Theme.of(context).colorScheme.error);
+        break;
+    }
 
     if (_euroMode) {
       //Euro Case
@@ -42,17 +82,19 @@ class StyledAmount extends StatelessWidget {
           maxLines: 1,
           text: TextSpan(
             children: [
+              if (_isNegative && value == 0)
+                TextSpan(text: '-', style: normalFontThemeNeg),
               TextSpan(
                 text: expMatch?.group(1) ?? "??",
-                style: _isNegative ? _errorTheme3 : _regularTheme3,
+                style: _isNegative ? normalFontThemeNeg : normalFontTheme,
               ),
               TextSpan(
                 text: expMatch?.group(2) ?? ".??",
-                style: _isNegative ? _errorTheme4 : _regularTheme4,
+                style: _isNegative ? smallFontThemeNeg : smallFontTheme,
               ),
               TextSpan(
                 text: symbol,
-                style: _isNegative ? _errorTheme3 : _regularTheme3,
+                style: _isNegative ? normalFontThemeNeg : normalFontTheme,
               ),
             ],
           ),
@@ -67,19 +109,27 @@ class StyledAmount extends StatelessWidget {
           children: [
             TextSpan(
               text: symbol,
-              style: _isNegative ? _errorTheme3 : _regularTheme3,
+              style: _isNegative ? normalFontThemeNeg : normalFontTheme,
             ),
+            if (_isNegative && value == 0)
+              TextSpan(text: '-', style: normalFontThemeNeg),
             TextSpan(
               text: expMatch?.group(1) ?? "??",
-              style: _isNegative ? _errorTheme3 : _regularTheme3,
+              style: _isNegative ? normalFontThemeNeg : normalFontTheme,
             ),
             TextSpan(
               text: expMatch?.group(2) ?? ".??",
-              style: _isNegative ? _errorTheme4 : _regularTheme4,
+              style: _isNegative ? smallFontThemeNeg : smallFontTheme,
             ),
           ],
         ),
       );
     }
   }
+}
+
+enum StyledFontSize {
+  standard,
+  compact,
+  maximize,
 }
