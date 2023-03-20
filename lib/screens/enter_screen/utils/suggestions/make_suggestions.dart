c9e0dbcf4ac61d85eaabca9239b995c492799498
Martin Mayer <ms-mayer@outlook.de>
2023-03-20 16:29:44 +0100

    feat: suggestion filter support

--- lib/screens/enter_screen/utils/suggestions/make_suggestions.dart
+++ lib/screens/enter_screen/utils/suggestions/make_suggestions.dart
@@ -1,11 +1,15 @@
 import 'package:linum/core/categories/models/category.dart';
+import 'package:linum/core/repeating/enums/repeat_interval.dart';
 import 'package:linum/screens/enter_screen/enums/input_flag.dart';
+import 'package:linum/screens/enter_screen/enums/parsable_date.dart';
 import 'package:linum/screens/enter_screen/models/suggestion.dart';
 import 'package:linum/screens/enter_screen/utils/input_parser.dart';
 import 'package:linum/screens/enter_screen/utils/suggestions/suggestion_functions.dart';
 
 Map<String, Suggestion> makeSuggestions(String text, int cursor, {
   bool Function(Category category)? categoryFilter,
+  bool Function(ParsableDate date)? dateFilter,
+  bool Function(RepeatInterval repeatInterval)? repeatFilter,
 }) {
   final textBefore = text.substring(0, cursor).split('#');
   // final textAfter = text.substring(cursor, text.length).split('#');
@@ -29,11 +33,17 @@ Map<String, Suggestion> makeSuggestions(String text, int cursor, {
     if (categorySuggestions.isNotEmpty) {
       return categorySuggestions;
     }
-    final dateSuggestions = suggestDate(parsed.item2);
+    final dateSuggestions = suggestDate(
+      parsed.item2,
+      filter: dateFilter,
+    );
     if (dateSuggestions.isNotEmpty) {
       return dateSuggestions;
     }
-    final repeatSuggestions = suggestRepeatInfo(parsed.item2);
+    final repeatSuggestions = suggestRepeatInfo(
+      parsed.item2,
+      filter: repeatFilter,
+    );
     if (repeatSuggestions.isNotEmpty) {
       return repeatSuggestions;
     }
@@ -44,10 +54,10 @@ Map<String, Suggestion> makeSuggestions(String text, int cursor, {
     case InputFlag.category:
       return suggestCategory(parsed.item2, filter: categoryFilter);
     case InputFlag.date:
-      return suggestDate(parsed.item2);
+      return suggestDate(parsed.item2, filter: dateFilter);
     case InputFlag.repeatInfo:
-      return suggestRepeatInfo(parsed.item2);
+      return suggestRepeatInfo(parsed.item2, filter: repeatFilter);
     default:
-      return suggestCategory(parsed.item2);
+      return suggestCategory(parsed.item2, filter: categoryFilter);
   }
 }
--- lib/screens/enter_screen/utils/suggestions/suggestion_functions.dart
+++ lib/screens/enter_screen/utils/suggestions/suggestion_functions.dart
@@ -2,9 +2,11 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:linum/core/categories/constants/standard_categories.dart';
 import 'package:linum/core/categories/models/category.dart';
 import 'package:linum/core/repeating/constants/standard_repeat_configs.dart';
+import 'package:linum/core/repeating/enums/repeat_interval.dart';
 import 'package:linum/screens/enter_screen/constants/input_flag_map.dart';
 import 'package:linum/screens/enter_screen/constants/parsable_date_map.dart';
 import 'package:linum/screens/enter_screen/constants/suggestion_defaults.dart';
+import 'package:linum/screens/enter_screen/enums/parsable_date.dart';
 import 'package:linum/screens/enter_screen/models/suggestion.dart';
 import 'package:linum/screens/enter_screen/utils/supported_dates.dart';
 import 'package:linum/screens/enter_screen/utils/supported_repeat_configs.dart';
@@ -58,11 +60,17 @@ Map<String, Suggestion> suggestCategory(String text, {
   return suggestions;
 }
 
-Map<String, Suggestion> suggestDate(String text) {
+Map<String, Suggestion> suggestDate(String text, {
+  bool Function(ParsableDate date)? filter,
+}) {
   final Map<String, Suggestion> suggestions = {};
   final lowercase = text.toLowerCase();
 
   for (final entry in supportedDates.entries) {
+    if (filter != null && !filter(entry.value)) {
+      continue;
+    }
+
     String? substr;
     if (entry.key.length > text.length) {
       substr = entry.key.substring(0, text.length);
@@ -78,11 +86,17 @@ Map<String, Suggestion> suggestDate(String text) {
   return suggestions;
 }
 
-Map<String, Suggestion> suggestRepeatInfo(String text) {
+Map<String, Suggestion> suggestRepeatInfo(String text, {
+  bool Function(RepeatInterval repeatInterval)? filter,
+}) {
   final Map<String, Suggestion> suggestions = {};
   final lowercase = text.toLowerCase();
 
   for (final entry in supportedRepeatIntervals.entries) {
+    if (filter != null && !filter(entry.value)) {
+      continue;
+    }
+
     String? substr;
     if (entry.key.length > text.length) {
       substr = entry.key.substring(0, text.length);
