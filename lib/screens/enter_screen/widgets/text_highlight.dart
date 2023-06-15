e0aed3db0a4a35de42a4591fb06f24313269f4bb
Martin Mayer <ms-mayer@outlook.de>
2023-06-15 11:51:25 +0200

    fix: layout not ready bug

--- lib/screens/enter_screen/widgets/text_highlight.dart
+++ lib/screens/enter_screen/widgets/text_highlight.dart
@@ -10,7 +10,8 @@ class TextHighlight extends StatelessWidget {
 
   Size _textSize(String text, TextStyle style) {
     final TextPainter textPainter = TextPainter(
-        text: TextSpan(text: text, style: style), maxLines: 1, textDirection: TextDirection.ltr,);
+        text: TextSpan(text: text, style: style), maxLines: 1, textDirection: TextDirection.ltr,)
+      ..layout();
     return textPainter.size;
   }
 
