8493d7ed105956acda7ea993283d7e3af920a30b
NightmindOfficial <mail@otismohr.de>
2023-07-05 21:27:54 +0200

    Del Firebase-Options.bat

--- get-firebase-options.bat
+++
@@ -1,2 +0,0 @@
-dart pub global activate flutterfire_cli
-flutterfire.bat configure -o lib/firebase/firebase_options.g.dart -p linum-dev -y --platforms="android,ios"
--- get-firebase-options.sh
+++ get-firebase-options.sh
@@ -1,2 +1,2 @@
 dart pub global activate flutterfire_cli
-flutterfire configure -o lib/firebase/firebase_options.g.dart -p linum-dev -y --platforms="android,ios"
+flutterfire.bat configure -o lib/firebase/firebase_options.g.dart -p linum-dev -y --platforms="android,ios"
