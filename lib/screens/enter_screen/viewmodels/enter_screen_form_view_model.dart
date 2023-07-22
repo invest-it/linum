6219280f0fda6f1559a61e7c69bbdae62c8f00b9
Martin Mayer <ms-mayer@outlook.de>
2023-07-22 15:47:12 +0200

    feat: linked serialtransaction data to enter_screen

--- lib/screens/enter_screen/viewmodels/enter_screen_form_view_model.dart
+++ lib/screens/enter_screen/viewmodels/enter_screen_form_view_model.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:linum/common/enums/entry_type.dart';
 import 'package:linum/core/account/services/account_settings_service.dart';
+import 'package:linum/core/balance/models/serial_transaction.dart';
+import 'package:linum/core/balance/models/transaction.dart';
 import 'package:linum/core/categories/constants/standard_categories.dart';
 import 'package:linum/core/repeating/constants/standard_repeat_configs.dart';
 import 'package:linum/core/repeating/enums/repeat_interval.dart';
@@ -47,7 +49,8 @@ class EnterScreenFormViewModel extends ChangeNotifier {
     _setupData(screenViewModel);
 
     final transaction = screenViewModel.initialTransaction;
-    withExistingData = transaction != null;
+    final serialTransaction = screenViewModel.initialSerialTransaction;
+    withExistingData = transaction != null || serialTransaction != null;
 
     final accountSettingsService
       = context.read<AccountSettingsService>();
@@ -67,23 +70,61 @@ class EnterScreenFormViewModel extends ChangeNotifier {
 
   void _setupData(EnterScreenViewModel screenViewModel) {
     final transaction = screenViewModel.initialTransaction;
-    final parentalSerialTransaction = screenViewModel.parentalSerialTransaction;
+    final serialTransaction = screenViewModel.initialSerialTransaction;
 
+    _entryType = screenViewModel.entryType;
+
+    assert(transaction == null || serialTransaction == null);
+
+    EnterScreenData? data;
+
+    if (transaction != null) {
+      data = _setupDataFromTransaction(transaction, screenViewModel);
+    }
+    if (serialTransaction != null) {
+      data = _setupDataFromSerialTransaction(serialTransaction);
+    }
+
+    _data = data ?? const EnterScreenData();
+  }
+
+  EnterScreenData _setupDataFromTransaction(
+      Transaction transaction,
+      EnterScreenViewModel screenViewModel,
+  ) {
+    final parentalSerialTransaction = screenViewModel.parentalSerialTransaction;
     final repeatInterval = getRepeatInterval(
       parentalSerialTransaction?.repeatDuration,
       parentalSerialTransaction?.repeatDurationType,
     );
 
-    _entryType = screenViewModel.entryType;
-
-    _data = EnterScreenData(
-      name: transaction?.name,
-      amount: transaction?.amount,
-      currency: standardCurrencies[transaction?.currency],
-      date: transaction?.date.toDate().toIso8601String(),
-      category: getCategory(transaction?.category, entryType: _entryType),
+    return EnterScreenData(
+      name: transaction.name,
+      amount: transaction.amount,
+      currency: standardCurrencies[transaction.currency],
+      date: transaction.date.toDate().toIso8601String(),
+      category: getCategory(transaction.category, entryType: _entryType),
       repeatConfiguration: repeatConfigurations[repeatInterval],
-      notes: transaction?.note,
+      notes: transaction.note,
+    );
+  }
+  EnterScreenData _setupDataFromSerialTransaction(
+      SerialTransaction serialTransaction,
+  ) {
+
+    final repeatInterval = getRepeatInterval(
+      serialTransaction.repeatDuration,
+      serialTransaction.repeatDurationType,
+    );
+
+    return EnterScreenData(
+      name: serialTransaction.name,
+      amount: serialTransaction.amount,
+      currency: standardCurrencies[serialTransaction.currency],
+      date: serialTransaction.startDate.toDate().toIso8601String(),
+      category: getCategory(serialTransaction.category, entryType: _entryType),
+      repeatConfiguration: repeatConfigurations[repeatInterval],
+      notes: serialTransaction.note,
     );
   }
 
