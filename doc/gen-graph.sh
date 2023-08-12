ea2222223489a03c672260a805d315fede90eea1
Martin Mayer <ms-mayer@outlook.de>
2023-08-12 16:56:09 +0200

    docs: automatic project structure analysis

---
+++ doc/gen-graph.sh
@@ -0,0 +1,5 @@
+lakos --no-tree -o structure.dot -m -i "{lib/generated/**,test/**}" --node-metrics  ../
+dot -Tsvg -o structure.svg structure.dot
+
+lakos --no-tree -o core-structure.dot -m -i  --node-metrics  ../lib/core
+dot -Tsvg -o core-structure.svg core-structure.dot
