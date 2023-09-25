9c56312025a5f06778aef12e58983e7a0e99b229
Martin Mayer <ms-mayer@outlook.de>
2023-09-25 20:19:22 +0200

    feature: linum-137-change-title-in-changemodeselection-based-on-intention

--- lib/screens/enter_screen/presentation/widgets/views/change_mode_selection_view.dart
+++ lib/screens/enter_screen/presentation/widgets/views/change_mode_selection_view.dart
@@ -2,6 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:linum/core/balance/enums/serial_transaction_change_type_enum.dart';
 import 'package:linum/generated/translation_keys.g.dart';
+import 'package:linum/screens/enter_screen/presentation/enums/edit_intention.dart';
 import 'package:linum/screens/enter_screen/presentation/view_models/enter_screen_view_model.dart';
 import 'package:linum/screens/enter_screen/presentation/widgets/buttons/change_mode_button.dart';
 import 'package:linum/screens/enter_screen/presentation/widgets/enter_screen_scaffold.dart';
@@ -14,6 +15,9 @@ class ChangeModeSelectionView extends StatelessWidget {
   Widget build(BuildContext context) {
     final viewModel = context.read<EnterScreenViewModel>();
 
+    final title = viewModel.intention == EditIntention.delete
+        ? tr(translationKeys.enterScreen.changeModeSelection.title.delete)
+        : tr(translationKeys.enterScreen.changeModeSelection.title.save);
 
     return EnterScreenScaffold(
       bodyHeight: 300,
@@ -26,7 +30,7 @@ class ChangeModeSelectionView extends StatelessWidget {
               width: double.maxFinite,
               padding: const EdgeInsets.only(bottom: 14.0),
               child: Text(
-                tr(translationKeys.enterScreen.changeModeSelection.title),
+                title,
                 style: Theme.of(context).textTheme.labelMedium?.copyWith(
                   color: Colors.black45,
                 ),
