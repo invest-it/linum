7bb4945cfa283e7da73ff15d92e51f5aca25620b
Martin Mayer <ms-mayer@outlook.de>
2023-10-10 13:41:41 +0200

    fix: gcloud secrets

--- android/android_cd.sh
+++ android/android_cd.sh
@@ -7,6 +7,8 @@ bundle install
 
 echo $(echo "$KEY_PROPERTIES" | base64 --decode) > key.properties
 
+gcloud secrets versions access latest --secret=linum-android-release-keystore-file --project=658687609050 > upload_keystore.jks
+
 flutter build appbundle
 
 cd ./android || exit
--- ios/ios_cd.sh
+++ ios/ios_cd.sh
@@ -8,6 +8,8 @@ bundle install
 GIT_BASIC_AUTH_TOKEN=$(echo -n "$GIT_USER_NAME:$GIT_ACCESS_TOKEN" | base64 | tr -d \\n)
 export GIT_BASIC_AUTH_TOKEN
 
+gcloud secrets versions access latest --secret=linum-ios-auth-file --project=658687609050 > ./AuthKey.p8
+
 flutter build ios --release --no-codesign
 
 export GIT_TERMINAL_PROMPT=1
