72b92e335d335817965055b6a634570b80727bc6
Martin Mayer <ms-mayer@outlook.de>
2023-10-13 20:31:43 +0200

    fix: possible bash error

--- android/android_cd.sh
+++ android/android_cd.sh
@@ -2,12 +2,15 @@
 
 cd ./android || exit
 
+KEY_PROPERTIES=$1
+
+
 gem install bundler:1.17.2
 bundle install
 
-echo "$KEY_PROPERTIES" | base64 --decode > key.properties
+echo "$KEY_PROPERTIES" | base64 --decode > ./key.properties
 
-cat key.properties
+cat ./key.properties
 
 gcloud secrets versions access latest --secret=linum-android-release-keystore-file --project=658687609050 --format "json" | jq -r .payload.data | base64 --decode > ./app/upload_keystore.jks
 cd ../
