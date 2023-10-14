0b3fa78cda7ec0d551805bd9f029aeb49028ec38
Martin Mayer <ms-mayer@outlook.de>
2023-10-14 21:18:50 +0200

    fix: padding

--- android/android_cd.sh
+++ android/android_cd.sh
@@ -11,7 +11,18 @@ bundle install
 echo "$KEY_PROPERTIES" | base64 --decode > ./key.properties
 
 KEY_STORE_BASE64=$(gcloud secrets versions access latest --secret=linum-android-release-keystore-file --project=658687609050 --format "json" | jq -r .payload.data)
-echo "$KEY_STORE_BASE64"==== fold -w 4 | sed '$ d' | tr -d '\n' | base64 --decode > ./app/upload_keystore.jks
+
+echo ${#KEY_STORE_BASE64}
+COUNT=$((${#KEY_STORE_BASE64}  % 4))
+echo $COUNT
+
+pad=""
+for ((i=1;i<=COUNT;i++))
+do
+ pad=$pad"="
+done
+
+echo "$KEY_STORE_BASE64$pad" | base64 -D > ./app/upload_keystore.jks
 
 
 cd ../
