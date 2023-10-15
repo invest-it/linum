e016c1f66905500f38320edc9d0033aa3e4bf3b8
Martin Mayer <ms-mayer@outlook.de>
2023-10-15 11:36:59 +0200

    fix: typo in file name

--- ios/ios_cd.sh
+++ ios/ios_cd.sh
@@ -8,7 +8,7 @@ bundle install
 GIT_BASIC_AUTH_TOKEN=$(echo -n "$GIT_USER_NAME:$GIT_ACCESS_TOKEN" | base64 | tr -d \\n)
 export GIT_BASIC_AUTH_TOKEN
 
-gcloud secrets versions access latest --secret=linum-ios-auth-file --project=658687609050 > ./AuthKey.p8
+gcloud secrets versions access latest --secret=linum-ios-auth-key-file --project=658687609050 > ./AuthKey.p8
 
 cd ../
 flutter build ios --release --no-codesign
