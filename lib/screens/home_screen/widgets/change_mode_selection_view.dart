0b95bae1c61b3fd92797bdc23c85ffe50e3e9d7a
Lukas Leitner <lukasleitner17@gmail.com>
2023-10-27 12:21:50 +0200

    remove unused imports and unused variable

--- lib/screens/home_screen/widgets/change_mode_selection_view.dart
+++ lib/screens/home_screen/widgets/change_mode_selection_view.dart
@@ -1,13 +1,10 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:linum/common/components/action_lip/viewmodels/action_lip_viewmodel.dart';
 import 'package:linum/common/components/dialogs/show_transaction_delete_dialog.dart';
 import 'package:linum/core/balance/enums/serial_transaction_change_type_enum.dart';
 import 'package:linum/core/balance/models/serial_transaction.dart';
 import 'package:linum/core/balance/models/transaction.dart';
 import 'package:linum/core/balance/services/balance_data_service.dart';
-import 'package:linum/core/design/layout/enums/screen_key.dart';
-import 'package:linum/core/design/layout/widgets/screen_skeleton.dart';
 import 'package:linum/generated/translation_keys.g.dart';
 import 'package:linum/screens/enter_screen/presentation/widgets/buttons/change_mode_button.dart';
 import 'package:provider/provider.dart';
