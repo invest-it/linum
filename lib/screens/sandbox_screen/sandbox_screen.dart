cadef97247630f9ada0f36ba359ff73cde64e612
NightmindOfficial <mail@otismohr.de>
2024-07-06 10:56:24 +0200

    Corrected Meta Head for Sandbox Screen

--- lib/screens/sandbox_screen/sandbox_screen.dart
+++ lib/screens/sandbox_screen/sandbox_screen.dart
@@ -1,8 +1,7 @@
-//  Statistics Screen - Screen allowing basic as well as complex visualization of transaction statistics
+//  Sandbox Screen - Allows for easier feature building and testing while in debug mode
 //
-//  Author: thebluebaronx (In the Future)
+//  Author: damattl
 //  Co-Author: n/a
-
 
 import 'package:cloud_firestore/cloud_firestore.dart' hide Transaction;
 import 'package:flutter/material.dart';
@@ -10,7 +9,6 @@ import 'package:linum/core/balance/models/transaction.dart';
 import 'package:linum/core/design/layout/widgets/screen_skeleton.dart';
 import 'package:linum/screens/enter_screen/enter_screen.dart';
 
-/// Page Index: 2
 class SandboxScreen extends StatelessWidget {
   const SandboxScreen({super.key});
 
