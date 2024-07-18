2280871db7c64163cced6259805fa98737a3cbe0
Martin Mayer <ms-mayer@outlook.de>
2024-07-18 21:32:52 +0200

    changing private key visibility

--- ios/ios_cd.sh
+++ ios/ios_cd.sh
@@ -6,7 +6,7 @@ gem install bundler:1.17.2
 bundle install
 
 echo "$FASTLANE_CERTS_REPO_KEY" > ./repo_key
-cat ./repo_key
+chmod 600 ./repo_key
 
 gcloud secrets versions access latest --secret=linum-ios-auth-key-file --project=658687609050 > ./AuthKey.p8
 
