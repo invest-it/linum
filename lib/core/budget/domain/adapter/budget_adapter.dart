72c779f4bcbd288487ec69c9f427b146e76b196a
Lukas Leitner <lukasleitner17@gmail.com>
2024-08-11 13:49:02 +0200

    add create, update and delete to the adapter interface

--- lib/core/budget/domain/adapter/budget_adapter.dart
+++ lib/core/budget/domain/adapter/budget_adapter.dart
@@ -1,3 +1,12 @@
-abstract class IBudgetAdapter {
+import 'package:linum/core/budget/domain/models/budget.dart';
+import 'package:linum/core/budget/domain/models/main_budget.dart';
 
+abstract class IBudgetAdapter {
+  Future<void> createBudget(Budget budget);
+  Future<void> updateBudget(Budget budget);
+  Future<void> deleteBudget(String id);
+
+  Future<void> createMainBudget(MainBudget budget);
+  Future<void> updateMainBudget(MainBudget budget);
+  Future<void> deleteMainBudget(String id);
 }
