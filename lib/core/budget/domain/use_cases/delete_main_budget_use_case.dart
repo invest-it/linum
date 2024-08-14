04a5a5539380ad63cb1fc5ad3a4ea4be12e95e2e
Samantha <samantha.carbajal@investit-academy.de>
2024-08-14 11:11:23 +0200

    feat: added MaindeleteBudgetUseCase Implemenation

---
+++ lib/core/budget/domain/use_cases/delete_main_budget_use_case.dart
@@ -0,0 +1,24 @@
+import 'package:linum/core/budget/domain/models/main_budget.dart';
+import 'package:linum/core/budget/domain/repositories/budget_repository.dart';
+import 'package:linum/core/budget/domain/use_cases/delete_time_span_use_case.dart';
+import 'package:linum/core/budget/enums/budget_change_mode.dart';
+
+abstract class DeleteMainBudgetUseCase {
+  void execute(MainBudget budget, DateTime selectedDate, BudgetChangeMode changeMode);
+}
+
+class DeleteMainBudgetUseCaseImpl implements DeleteMainBudgetUseCase {
+  final IBudgetRepository repository;
+  final DeleteTimeSpanUseCase<MainBudget> deleteTimeSpanUseCase;
+
+  DeleteMainBudgetUseCaseImpl({
+    required this.repository,
+  }): deleteTimeSpanUseCase = DeleteTimeSpanUseCase(
+    deleteSpan: repository.removeMainBudget, createSpan: repository.createMainBudget,
+  );
+
+  @override
+  void execute(MainBudget budget, DateTime selectedDate, BudgetChangeMode changeMode) {
+    deleteTimeSpanUseCase.execute(budget, selectedDate, changeMode);
+  }
+}
