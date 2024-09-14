92d99d2fad701178ced90247563224429952efee
Samantha <samantha.carbajal@investit-academy.de>
2024-09-14 15:12:33 +0200

    refactor: converted sub_budget tile to stateful widget

--- lib/screens/budget_screen/pages/budget_view_screen/widgets/sub_budget_tile.dart
+++ lib/screens/budget_screen/pages/budget_view_screen/widgets/sub_budget_tile.dart
@@ -4,27 +4,38 @@ import 'package:linum/features/currencies/core/utils/currency_formatter.dart';
 import 'package:linum/features/currencies/settings/presentation/currency_settings_service.dart';
 import 'package:provider/provider.dart';
 
-
 class BudgetViewData {
   final String name;
   final double expenses;
   final double cap;
   final List<String> categories;
 
-  BudgetViewData({required this.name, required this.expenses, required this.cap, required this.categories});
+  BudgetViewData(
+      {required this.name,
+      required this.expenses,
+      required this.cap,
+      required this.categories});
 }
 
-class SubBudgetTile extends StatelessWidget {
+class SubBudgetTile extends StatefulWidget {
   final BudgetViewData budgetData;
-
   const SubBudgetTile({super.key, required this.budgetData});
 
+  @override
+  State<SubBudgetTile> createState() => _SubBudgetTileState();
+}
+
+class _SubBudgetTileState extends State<SubBudgetTile> {
+  var isOpen = false;
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final formatter = CurrencyFormatter(
       context.locale,
-      symbol: context.watch<ICurrencySettingsService>().getStandardCurrency().symbol,
+      symbol: context
+          .watch<ICurrencySettingsService>()
+          .getStandardCurrency()
+          .symbol,
     );
 
     return Card.outlined(
@@ -37,32 +48,40 @@ class SubBudgetTile extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  budgetData.name,
+                  widget.budgetData.name,
                   style: theme.textTheme.labelLarge?.copyWith(
                     color: theme.colorScheme.secondary,
                   ),
                 ),
-                IconButton(onPressed: () {}, icon: const Icon(Icons.expand_more)),
+                IconButton(
+                    onPressed: () {
+                      setState(() {
+                        isOpen = !isOpen;
+                      });
+                    },
+                    icon: const Icon(Icons.expand_more)),
               ],
             ),
-
             Padding(
               padding: const EdgeInsets.only(top: 12.0, bottom: 4.0),
               child: LinearProgressIndicator(
                 backgroundColor: Colors.black12,
                 color: theme.colorScheme.primary,
-                value: budgetData.expenses / budgetData.cap,
-                borderRadius:
-                const BorderRadius.all(Radius.circular(10.0)),
+                value: widget.budgetData.expenses / widget.budgetData.cap,
+                borderRadius: const BorderRadius.all(Radius.circular(10.0)),
               ),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("${formatter.format(budgetData.cap - budgetData.expenses)} remaining", style: theme.textTheme.labelMedium),
-                Text(formatter.format(budgetData.cap), style: theme.textTheme.labelMedium),
+                Text(
+                    "${formatter.format(widget.budgetData.cap - widget.budgetData.expenses)} remaining",
+                    style: theme.textTheme.labelMedium),
+                Text(formatter.format(widget.budgetData.cap),
+                    style: theme.textTheme.labelMedium),
               ],
             ),
+            if (isOpen) const Text("Hola mucho gusto"),
           ],
         ),
       ),
